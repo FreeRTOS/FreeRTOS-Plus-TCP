4537de4589832753c9e7e8660aa2baae12eb2ef9
AniruddhaKanhere <kanherea@amazon.com>
2020-10-21 21:21:09 -0700

    Uncrustify header files

--- include/NetworkBufferManagement.h
+++ include/NetworkBufferManagement.h
@@ -24,53 +24,53 @@
  */
 
 #ifndef NETWORK_BUFFER_MANAGEMENT_H
-	#define NETWORK_BUFFER_MANAGEMENT_H
+    #define NETWORK_BUFFER_MANAGEMENT_H
 
-	#ifdef __cplusplus
-	extern "C" {
-	#endif
+    #ifdef __cplusplus
+        extern "C" {
+    #endif
 
 /* NOTE PUBLIC API FUNCTIONS. */
-	BaseType_t xNetworkBuffersInitialise( void );
-	NetworkBufferDescriptor_t * pxGetNetworkBufferWithDescriptor( size_t xRequestedSizeBytes,
-																  TickType_t xBlockTimeTicks );
+    BaseType_t xNetworkBuffersInitialise( void );
+    NetworkBufferDescriptor_t * pxGetNetworkBufferWithDescriptor( size_t xRequestedSizeBytes,
+                                                                  TickType_t xBlockTimeTicks );
 
 /* The definition of the below function is only available if BufferAllocation_2.c has been linked into the source. */
-	NetworkBufferDescriptor_t * pxNetworkBufferGetFromISR( size_t xRequestedSizeBytes );
-	void vReleaseNetworkBufferAndDescriptor( NetworkBufferDescriptor_t * const pxNetworkBuffer );
+    NetworkBufferDescriptor_t * pxNetworkBufferGetFromISR( size_t xRequestedSizeBytes );
+    void vReleaseNetworkBufferAndDescriptor( NetworkBufferDescriptor_t * const pxNetworkBuffer );
 
 /* The definition of the below function is only available if BufferAllocation_2.c has been linked into the source. */
-	BaseType_t vNetworkBufferReleaseFromISR( NetworkBufferDescriptor_t * const pxNetworkBuffer );
-	uint8_t * pucGetNetworkBuffer( size_t *pxRequestedSizeBytes );
-	void vReleaseNetworkBuffer( uint8_t *pucEthernetBuffer );
+    BaseType_t vNetworkBufferReleaseFromISR( NetworkBufferDescriptor_t * const pxNetworkBuffer );
+    uint8_t * pucGetNetworkBuffer( size_t * pxRequestedSizeBytes );
+    void vReleaseNetworkBuffer( uint8_t * pucEthernetBuffer );
 
 /* Get the current number of free network buffers. */
-	UBaseType_t uxGetNumberOfFreeNetworkBuffers( void );
+    UBaseType_t uxGetNumberOfFreeNetworkBuffers( void );
 
 /* Get the lowest number of free network buffers. */
-	UBaseType_t uxGetMinimumFreeNetworkBuffers( void );
+    UBaseType_t uxGetMinimumFreeNetworkBuffers( void );
 
 /* Copy a network buffer into a bigger buffer. */
-	NetworkBufferDescriptor_t * pxDuplicateNetworkBufferWithDescriptor( const NetworkBufferDescriptor_t * const pxNetworkBuffer,
-																		size_t uxNewLength );
+    NetworkBufferDescriptor_t * pxDuplicateNetworkBufferWithDescriptor( const NetworkBufferDescriptor_t * const pxNetworkBuffer,
+                                                                        size_t uxNewLength );
 
 /* Increase the size of a Network Buffer.
-In case BufferAllocation_2.c is used, the new space must be allocated. */
-	NetworkBufferDescriptor_t * pxResizeNetworkBufferWithDescriptor( NetworkBufferDescriptor_t * pxNetworkBuffer,
-																	 size_t xNewSizeBytes );
+ * In case BufferAllocation_2.c is used, the new space must be allocated. */
+    NetworkBufferDescriptor_t * pxResizeNetworkBufferWithDescriptor( NetworkBufferDescriptor_t * pxNetworkBuffer,
+                                                                     size_t xNewSizeBytes );
 
-	#if ipconfigTCP_IP_SANITY
+    #if ipconfigTCP_IP_SANITY
 
-		/*
-		* Check if an address is a valid pointer to a network descriptor
-		* by looking it up in the array of network descriptors
-		*/
-		UBaseType_t bIsValidNetworkDescriptor( const NetworkBufferDescriptor_t * pxDesc );
-		BaseType_t prvIsFreeBuffer( const NetworkBufferDescriptor_t *pxDescr );
-	#endif
+        /*
+         * Check if an address is a valid pointer to a network descriptor
+         * by looking it up in the array of network descriptors
+         */
+        UBaseType_t bIsValidNetworkDescriptor( const NetworkBufferDescriptor_t * pxDesc );
+        BaseType_t prvIsFreeBuffer( const NetworkBufferDescriptor_t * pxDescr );
+    #endif
 
-	#ifdef __cplusplus
-	} /* extern "C" */
-	#endif
+    #ifdef __cplusplus
+        } /* extern "C" */
+    #endif
 
 #endif /* NETWORK_BUFFER_MANAGEMENT_H */
