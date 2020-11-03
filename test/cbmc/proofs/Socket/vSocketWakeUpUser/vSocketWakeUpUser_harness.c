cf626c2cb3bba79e95e2543edaf2cdd5c8fc0ecc
AniruddhaKanhere <kanherea@amazon.com>
2020-11-02 17:02:09 -0800

    Add harness

--- test/cbmc/proofs/Socket/vSocketWakeUpUser/vSocketWakeUpUser_harness.c
+++ test/cbmc/proofs/Socket/vSocketWakeUpUser/vSocketWakeUpUser_harness.c
@@ -4,7 +4,7 @@
 
 /* FreeRTOS includes. */
 #include "FreeRTOS.h"
-#include "list.h"
+#include "semphr.h"
 #include "event_groups.h"
 
 /* FreeRTOS+TCP includes. */
@@ -14,39 +14,18 @@
 
 #include "memory_assignments.c"
 
-uint16_t prvGetPrivatePortNumber( BaseType_t xProtocol );
-
-
-uint16_t prvGetPrivatePortNumber( BaseType_t xProtocol )
+/* The memory safety of xQueueGenericSend has been proved before.
+ * See github.com/FreeRTOS/FreeRTOS/FreeRTOS/Test/CBMC/proofs/Queue/QueueGenericSend.
+ */
+BaseType_t xQueueGenericSend( QueueHandle_t xQueue,
+                              const void * const pvItemToQueue,
+                              TickType_t xTicksToWait,
+                              const BaseType_t xCopyPosition )
 {
-    uint16_t usResult;
+    BaseType_t xResult;
 
-    return usResult;
-}
-
-BaseType_t xIPIsNetworkTaskReady( void )
-{
-    /* Return true saying that the task is ready. */
-    return pdTRUE;
-}
-
-
-/* Random number generator provided by the application. In our case, CBMC provides
- * an indeterministic value. */
-BaseType_t xApplicationGetRandomNumber( uint32_t * pulNumber )
-{
-    __CPROVER_assert( pulNumber != NULL, "Argument to xApplicationGetRandomNumber cannot be NULL" );
-
-    if( nondet_bool() )
-    {
-        *pulNumber = nondet_uint32_t();
-        return 1;
-    }
-    else
-    {
-        *pulNumber = NULL;
-        return 0;
-    }
+    /* Return any random value. */
+    return xResult;
 }
 
 void harness()
@@ -55,6 +34,14 @@ void harness()
 
     __CPROVER_assume( pxSocket != NULL );
     __CPROVER_assume( pxSocket != FREERTOS_INVALID_SOCKET );
+
+    pxSocket->pxUserWakeCallback = safeMalloc( sizeof( SocketWakeupCallback_t ) );
+    pxSocket->pxSocketSet = safeMalloc( sizeof( struct xSOCKET_SET ) );
+
+    if( pxSocket->pxSocketSet != NULL )
+    {
+        pxSocket->pxSocketSet->xSelectGroup = safeMalloc( sizeof( EventGroupHandle_t ) );
+    }
 
     /* Call to init the socket list. */
     vNetworkSocketsInit();
