62a664fee332f7355f4d1fc9a1403f3af442503b
AniruddhaKanhere <kanherea@amazon.com>
2020-11-04 12:44:40 -0800

    Update proof

--- test/cbmc/proofs/Socket/vSocketWakeUpUser/vSocketWakeUpUser_harness.c
+++ test/cbmc/proofs/Socket/vSocketWakeUpUser/vSocketWakeUpUser_harness.c
@@ -15,61 +15,53 @@
 
 #include "memory_assignments.c"
 
+
+/*********************************************************************************
+ *
+ * The below structure definitions are just copy pasted from the FreeRTOS-Kernel.
+ * To understand the proof, you need not understand the structures and they can
+ * be ignored safely.
+ *
+ ********************************************************************************/
+
 /* Define the bits used by Kernel. */
 #define eventEVENT_BITS_CONTROL_BYTES    0xff000000UL
 
-typedef struct EventGroupDef_t
-{
-    EventBits_t uxEventBits;
-    List_t xTasksWaitingForBits; /*< List of tasks waiting for a bit to be set. */
-
-    #if ( configUSE_TRACE_FACILITY == 1 )
-        UBaseType_t uxEventGroupNumber;
-    #endif
-
-    #if ( ( configSUPPORT_STATIC_ALLOCATION == 1 ) && ( configSUPPORT_DYNAMIC_ALLOCATION == 1 ) )
-        uint8_t ucStaticallyAllocated; /*< Set to pdTRUE if the event group is statically allocated to ensure no attempt is made to free the memory. */
-    #endif
-} EventGroup_t;
-
-/* Definition of QueuePointers as required for the proof. */
 typedef struct QueuePointers
 {
-    int8_t * pcTail;     /*< Points to the byte at the end of the queue storage area.  Once more byte is allocated than necessary to store the queue items, this is used as a marker. */
-    int8_t * pcReadFrom; /*< Points to the last place that a queued item was read from when the structure is used as a queue. */
+    int8_t * pcTail;
+    int8_t * pcReadFrom;
 } QueuePointers_t;
 
-/* Definition of SemaphoreData as required for the proof. */
 typedef struct SemaphoreData
 {
-    TaskHandle_t xMutexHolder;        /*< The handle of the task that holds the mutex. */
-    UBaseType_t uxRecursiveCallCount; /*< Maintains a count of the number of times a recursive mutex has been recursively 'taken' when the structure is used as a mutex. */
+    TaskHandle_t xMutexHolder;
+    UBaseType_t uxRecursiveCallCount;
 } SemaphoreData_t;
 
-/* Definition of QueueDefinition as required for the proof. */
-typedef struct QueueDefinition /* The old naming convention is used to prevent breaking kernel aware debuggers. */
+typedef struct QueueDefinition
 {
-    int8_t * pcHead;           /*< Points to the beginning of the queue storage area. */
-    int8_t * pcWriteTo;        /*< Points to the free next place in the storage area. */
+    int8_t * pcHead;
+    int8_t * pcWriteTo;
 
     union
     {
-        QueuePointers_t xQueue;     /*< Data required exclusively when this structure is used as a queue. */
-        SemaphoreData_t xSemaphore; /*< Data required exclusively when this structure is used as a semaphore. */
+        QueuePointers_t xQueue;
+        SemaphoreData_t xSemaphore;
     } u;
 
-    List_t xTasksWaitingToSend;             /*< List of tasks that are blocked waiting to post onto this queue.  Stored in priority order. */
-    List_t xTasksWaitingToReceive;          /*< List of tasks that are blocked waiting to read from this queue.  Stored in priority order. */
+    List_t xTasksWaitingToSend;
+    List_t xTasksWaitingToReceive;
 
-    volatile UBaseType_t uxMessagesWaiting; /*< The number of items currently in the queue. */
-    UBaseType_t uxLength;                   /*< The length of the queue defined as the number of items it will hold, not the number of bytes. */
-    UBaseType_t uxItemSize;                 /*< The size of each items that the queue will hold. */
+    volatile UBaseType_t uxMessagesWaiting;
+    UBaseType_t uxLength;
+    UBaseType_t uxItemSize;
 
-    volatile int8_t cRxLock;                /*< Stores the number of items received from the queue (removed from the queue) while the queue was locked.  Set to queueUNLOCKED when the queue is not locked. */
-    volatile int8_t cTxLock;                /*< Stores the number of items transmitted to the queue (added to the queue) while the queue was locked.  Set to queueUNLOCKED when the queue is not locked. */
+    volatile int8_t cRxLock;
+    volatile int8_t cTxLock;
 
     #if ( ( configSUPPORT_STATIC_ALLOCATION == 1 ) && ( configSUPPORT_DYNAMIC_ALLOCATION == 1 ) )
-        uint8_t ucStaticallyAllocated; /*< Set to pdTRUE if the memory used by the queue was statically allocated to ensure no attempt is made to free the memory. */
+        uint8_t ucStaticallyAllocated;
     #endif
 
     #if ( configUSE_QUEUE_SETS == 1 )
@@ -82,6 +74,10 @@ typedef struct QueueDefinition /* The old naming convention is used to prevent b
     #endif
 } xQUEUE;
 
+/********************************************************/
+/********* End Kernel cut-paste section *****************/
+/********************************************************/
+
 
 /* The memory safety of xQueueGenericSend has been proved before.
  * See github.com/FreeRTOS/FreeRTOS/FreeRTOS/Test/CBMC/proofs/Queue/QueueGenericSend.
@@ -93,9 +89,12 @@ BaseType_t xQueueGenericSend( QueueHandle_t xQueue,
 {
     BaseType_t xResult;
 
+    /* These asserts are copied over from the original function itself. */
     __CPROVER_assert( xQueue != NULL, "xQueue cannot be NULL" );
-    __CPROVER_assert( !( ( pvItemToQueue == NULL ) && ( xQueue->uxItemSize != ( UBaseType_t ) 0U ) ), "If itemsize is non-zero, then pvItemToQueue cannot be NULL." );
-    __CPROVER_assert( !( ( xCopyPosition == queueOVERWRITE ) && ( xQueue->uxLength != 1 ) ), "If length is one, then check the copy position" );
+    __CPROVER_assert( !( ( pvItemToQueue == NULL ) && ( xQueue->uxItemSize != ( UBaseType_t ) 0U ) ),
+                      "If itemsize is non-zero, then pvItemToQueue cannot be NULL." );
+    __CPROVER_assert( !( ( xCopyPosition == queueOVERWRITE ) && ( xQueue->uxLength != 1 ) ),
+                      "If length is one, then check the copy position" );
 
     /* Return any random value. */
     return xResult;
@@ -105,14 +104,16 @@ BaseType_t xQueueGenericSend( QueueHandle_t xQueue,
 EventBits_t xEventGroupSetBits( EventGroupHandle_t xEventGroup,
                                 const EventBits_t uxBitsToSet )
 {
-    EventGroup_t * TempEventGroup = xEventGroup;
+    EventBits_t uxReturnBits;
 
-    /* Check the user is not attempting to set the bits used by the kernel
-     * itself. */
-    __CPROVER_assert( xEventGroup != NULL, "The event group cannot be NULL" );
-    __CPROVER_assert( ( uxBitsToSet & eventEVENT_BITS_CONTROL_BYTES ) == 0, "Cannot set Kernel bits" );
+    /* The below asserts are copied over from the original function itself. */
+    __CPROVER_assert( xEventGroup != NULL,
+                      "The event group cannot be NULL" );
+    __CPROVER_assert( ( uxBitsToSet & eventEVENT_BITS_CONTROL_BYTES ) == 0,
+                      "Cannot set Kernel bits" );
 
-    return TempEventGroup->uxEventBits;
+    /* Return any random value. */
+    return uxReturnBits;
 }
 
 
@@ -140,7 +141,7 @@ void harness()
     if( pxSocket->pxUserSemaphore != NULL )
     {
         /* The itemsize must be zero since this queue will act as a semaphore.  */
-        __CPROVER_assume(pxSocket->pxUserSemaphore->uxItemSize == 0);
+        __CPROVER_assume( pxSocket->pxUserSemaphore->uxItemSize == 0 );
     }
 
     pxSocket->xEventGroup = safeMalloc( sizeof( struct EventGroupDef_t ) );
