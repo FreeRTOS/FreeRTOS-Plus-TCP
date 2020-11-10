a8546d8dfb56fd3d81aeca5322f8b264b1dd1a99
AniruddhaKanhere <kanherea@amazon.com>
2020-11-09 18:00:04 -0800

    Update v2

--- test/cbmc/proofs/TCPWin/vTCPWindowDestroy/vTCPWindowDestroy_harness.c
+++ test/cbmc/proofs/TCPWin/vTCPWindowDestroy/vTCPWindowDestroy_harness.c
@@ -5,25 +5,46 @@
 /* FreeRTOS+TCP includes. */
 #include "FreeRTOS_IP.h"
 #include "FreeRTOS_IP_Private.h"
-#include "FreeRTOS_ARP.h"
+#include "FreeRTOS_TCP_WIN.h"
 
-/*We assume that the pxGetNetworkBufferWithDescriptor function is implemented correctly and returns a valid data structure. */
-/*This is the mock to mimic the correct expected behavior. If this allocation fails, this might invalidate the proof. */
-NetworkBufferDescriptor_t * pxGetNetworkBufferWithDescriptor( size_t xRequestedSizeBytes,
-                                                              TickType_t xBlockTimeTicks )
-{
-    NetworkBufferDescriptor_t * pxNetworkBuffer = ( NetworkBufferDescriptor_t * ) malloc( sizeof( NetworkBufferDescriptor_t ) );
+#define NUM_OF_SEGMENTS 1
 
-    __CPROVER_assume( pxNetworkBuffer != NULL );
+const TCPSegment_t xRxSegmentListItem[ NUM_OF_SEGMENTS ];
+const TCPSegment_t xTxSegmentListItem[ NUM_OF_SEGMENTS ];
 
-    pxNetworkBuffer->pucEthernetBuffer = malloc( xRequestedSizeBytes );
-    __CPROVER_assume( pxNetworkBuffer->pucEthernetBuffer != NULL );
+static void vListInsertGeneric( List_t * const pxList,
+                                    ListItem_t * const pxNewListItem,
+                                    MiniListItem_t * const pxWhere )
+    {
+        /* Insert a new list item into pxList, it does not sort the list,
+         * but it puts the item just before xListEnd, so it will be the last item
+         * returned by listGET_HEAD_ENTRY() */
+        pxNewListItem->pxNext = ( struct xLIST_ITEM * configLIST_VOLATILE ) pxWhere;
+        pxNewListItem->pxPrevious = pxWhere->pxPrevious;
+        pxWhere->pxPrevious->pxNext = pxNewListItem;
+        pxWhere->pxPrevious = pxNewListItem;
 
-    pxNetworkBuffer->xDataLength = xRequestedSizeBytes;
-    return pxNetworkBuffer;
-}
+        /* Remember which list the item is in. */
+        listLIST_ITEM_CONTAINER( pxNewListItem ) = ( struct xLIST * configLIST_VOLATILE ) pxList;
+
+        ( pxList->uxNumberOfItems )++;
+    }
+
 
 void harness()
 {
-    vARPAgeCache();
+    /* Create a TCP Window to be destroyed and fill it with random data. */
+    TCPWindow_t xWindow;
+
+    /* Initialise the Rx and Tx lists of the window. */
+    vListInitialise( &xWindow.xRxSegments );
+    vListInitialise( &xWindow.xTxSegments );
+
+    for( int i=0; i < NUM_OF_SEGMENTS; i++ )
+    {
+        vListInsertGeneric( &xWindow.xRxSegments, &( xRxSegmentListItem[ i ].xSegmentItem ), &xWindow.xRxSegments.xListEnd );
+        //vListInsertFifo( &xWindow.xTxSegments, &( xTxSegmentListItem[ i ].xSegmentItem ) );
+    }
+
+    vTCPWindowDestroy( &xWindow );
 }
