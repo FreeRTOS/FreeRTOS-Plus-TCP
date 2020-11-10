c8d100173b608bd3ac1d74e232d9cbf5cc571a38
AniruddhaKanhere <kanherea@amazon.com>
2020-11-10 12:55:53 -0800

    Uncrustify

--- test/cbmc/proofs/TCPWin/vTCPWindowDestroy/vTCPWindowDestroy_harness.c
+++ test/cbmc/proofs/TCPWin/vTCPWindowDestroy/vTCPWindowDestroy_harness.c
@@ -7,28 +7,28 @@
 #include "FreeRTOS_IP_Private.h"
 #include "FreeRTOS_TCP_WIN.h"
 
-#define NUM_OF_SEGMENTS 3
+#define NUM_OF_SEGMENTS    3
 
 TCPSegment_t xRxSegmentListItem[ NUM_OF_SEGMENTS ];
 TCPSegment_t xTxSegmentListItem[ NUM_OF_SEGMENTS ];
 
 static void vListInsertGeneric( List_t * const pxList,
-                                    ListItem_t * const pxNewListItem,
-                                    MiniListItem_t * const pxWhere )
-    {
-        /* Insert a new list item into pxList, it does not sort the list,
-         * but it puts the item just before xListEnd, so it will be the last item
-         * returned by listGET_HEAD_ENTRY() */
-        pxNewListItem->pxNext = ( struct xLIST_ITEM * configLIST_VOLATILE ) pxWhere;
-        pxNewListItem->pxPrevious = pxWhere->pxPrevious;
-        pxWhere->pxPrevious->pxNext = pxNewListItem;
-        pxWhere->pxPrevious = pxNewListItem;
+                                ListItem_t * const pxNewListItem,
+                                MiniListItem_t * const pxWhere )
+{
+    /* Insert a new list item into pxList, it does not sort the list,
+     * but it puts the item just before xListEnd, so it will be the last item
+     * returned by listGET_HEAD_ENTRY() */
+    pxNewListItem->pxNext = ( struct xLIST_ITEM * configLIST_VOLATILE ) pxWhere;
+    pxNewListItem->pxPrevious = pxWhere->pxPrevious;
+    pxWhere->pxPrevious->pxNext = pxNewListItem;
+    pxWhere->pxPrevious = pxNewListItem;
 
-        /* Remember which list the item is in. */
-        listLIST_ITEM_CONTAINER( pxNewListItem ) = ( struct xLIST * configLIST_VOLATILE ) pxList;
+    /* Remember which list the item is in. */
+    listLIST_ITEM_CONTAINER( pxNewListItem ) = ( struct xLIST * configLIST_VOLATILE ) pxList;
 
-        ( pxList->uxNumberOfItems )++;
-    }
+    ( pxList->uxNumberOfItems )++;
+}
 
 extern List_t xSegmentList;
 
@@ -46,7 +46,7 @@ void harness()
     vListInitialise( &xWindow.xRxSegments );
     vListInitialise( &xWindow.xTxSegments );
 
-    for( int i=0; i < NUM_OF_SEGMENTS; i++ )
+    for( int i = 0; i < NUM_OF_SEGMENTS; i++ )
     {
         xRxSegmentListItem[ i ].xSegmentItem.pvOwner = &( xRxSegmentListItem[ i ] );
 
