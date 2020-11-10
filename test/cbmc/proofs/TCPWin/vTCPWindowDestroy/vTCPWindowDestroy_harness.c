2463875f07e1091b72cd7e805684bb2ee017ba0f
AniruddhaKanhere <kanherea@amazon.com>
2020-11-10 13:37:29 -0800

    Add comments

--- test/cbmc/proofs/TCPWin/vTCPWindowDestroy/vTCPWindowDestroy_harness.c
+++ test/cbmc/proofs/TCPWin/vTCPWindowDestroy/vTCPWindowDestroy_harness.c
@@ -7,61 +7,54 @@
 #include "FreeRTOS_IP_Private.h"
 #include "FreeRTOS_TCP_WIN.h"
 
+/* Randomly chosen Number of segments */
 #define NUM_OF_SEGMENTS    3
 
+/* Rx/Tx list items to be used in the proof. */
 TCPSegment_t xRxSegmentListItem[ NUM_OF_SEGMENTS ];
 TCPSegment_t xTxSegmentListItem[ NUM_OF_SEGMENTS ];
 
-static void vListInsertGeneric( List_t * const pxList,
+/* Definition of this function in FreeRTOS_TCP_WIN.c. */
+extern void vListInsertGeneric( List_t * const pxList,
                                 ListItem_t * const pxNewListItem,
-                                MiniListItem_t * const pxWhere )
-{
-    /* Insert a new list item into pxList, it does not sort the list,
-     * but it puts the item just before xListEnd, so it will be the last item
-     * returned by listGET_HEAD_ENTRY() */
-    pxNewListItem->pxNext = ( struct xLIST_ITEM * configLIST_VOLATILE ) pxWhere;
-    pxNewListItem->pxPrevious = pxWhere->pxPrevious;
-    pxWhere->pxPrevious->pxNext = pxNewListItem;
-    pxWhere->pxPrevious = pxNewListItem;
+                                MiniListItem_t * const pxWhere );
 
-    /* Remember which list the item is in. */
-    listLIST_ITEM_CONTAINER( pxNewListItem ) = ( struct xLIST * configLIST_VOLATILE ) pxList;
-
-    ( pxList->uxNumberOfItems )++;
-}
-
+/* Segment List is defined in FreeRTOS_TCP_WIN.c */
 extern List_t xSegmentList;
 
 void harness()
 {
     /* Create a TCP Window to be destroyed and fill it with random data. */
     TCPWindow_t xWindow;
-    uint32_t ulSequenceNumber;
-    int32_t lCount;
-    BaseType_t xIsForRx = nondet_bool();
 
+    /* Ititialise the segment list. */
     vListInitialise( &xSegmentList );
 
     /* Initialise the Rx and Tx lists of the window. */
     vListInitialise( &xWindow.xRxSegments );
     vListInitialise( &xWindow.xTxSegments );
 
+    /* Below loop fills in various segments in the Rx/Tx list of the window. */
     for( int i = 0; i < NUM_OF_SEGMENTS; i++ )
     {
+        /********************** Fill in Rx segments ********************/
         xRxSegmentListItem[ i ].xSegmentItem.pvOwner = &( xRxSegmentListItem[ i ] );
 
+        /* Assume the container of the queueitem is NULL. */
         __CPROVER_assume( xRxSegmentListItem[ i ].xQueueItem.pxContainer == NULL );
 
         vListInsertGeneric( &xWindow.xRxSegments, &( xRxSegmentListItem[ i ].xSegmentItem ), &xWindow.xRxSegments.xListEnd );
 
 
-
+        /********************** Fill in Tx segments ********************/
         xTxSegmentListItem[ i ].xSegmentItem.pvOwner = &( xTxSegmentListItem[ i ] );
 
+        /* Assume the container of the queueitem is NULL. */
         __CPROVER_assume( xTxSegmentListItem[ i ].xQueueItem.pxContainer == NULL );
 
         vListInsertGeneric( &xWindow.xTxSegments, &( xTxSegmentListItem[ i ].xSegmentItem ), &xWindow.xTxSegments.xListEnd );
     }
 
+    /* Call the function. */
     vTCPWindowDestroy( &xWindow );
 }
