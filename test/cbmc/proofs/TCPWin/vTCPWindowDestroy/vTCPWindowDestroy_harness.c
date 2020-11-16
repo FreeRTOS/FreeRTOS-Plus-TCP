e7296bec5c00c1c222c24f75db212a49c88de168
Aniruddha Kanhere <60444055+AniruddhaKanhere@users.noreply.github.com>
2020-11-16 10:49:59 -0800

    Add CBMC proof for vTCPWindowDestroy (#66)
    * Add basic infrastructure
    
    * Update v2
    
    * Update the Proof and config
    
    * Uncrustify
    
    * Add comments
    
    * Remove static
    
    * Uncrustify
    
    * Spell-check
    
    * Remove one implicit assumption
    
    * Remove assume and replace with assignment
    
    * Update proof to use flags for static functions
    
    * Uncrustify
    
    * Missed one function to replace
    
    * Add comment for clarificaiton

---
+++ test/cbmc/proofs/TCPWin/vTCPWindowDestroy/vTCPWindowDestroy_harness.c
@@ -0,0 +1,69 @@
+/* FreeRTOS includes. */
+#include "FreeRTOS.h"
+#include "list.h"
+
+/* FreeRTOS+TCP includes. */
+#include "FreeRTOS_IP.h"
+#include "FreeRTOS_IP_Private.h"
+#include "FreeRTOS_TCP_WIN.h"
+
+/* Randomly chosen Number of segments */
+#define NUM_OF_SEGMENTS    3
+
+/* Rx/Tx list items to be used in the proof. */
+TCPSegment_t xRxSegmentListItem[ NUM_OF_SEGMENTS ];
+TCPSegment_t xTxSegmentListItem[ NUM_OF_SEGMENTS ];
+
+/* Definition of this function in FreeRTOS_TCP_WIN.c. */
+void __CPROVER_file_local_FreeRTOS_TCP_WIN_c_vListInsertGeneric( List_t * const pxList,
+                                                                 ListItem_t * const pxNewListItem,
+                                                                 MiniListItem_t * const pxWhere );
+
+/* Segment List is defined in FreeRTOS_TCP_WIN.c */
+extern List_t xSegmentList;
+
+void harness()
+{
+    uint32_t temp;
+
+    /* Choose any value between 0 and NUM_OF_SEGMENTS. */
+    __CPROVER_assume( temp <= NUM_OF_SEGMENTS );
+
+    /* Create a TCP Window to be destroyed and fill it with random data. */
+    TCPWindow_t xWindow;
+
+    /* Initialise the segment list. */
+    vListInitialise( &xSegmentList );
+
+    /* Initialise the Rx and Tx lists of the window. */
+    vListInitialise( &xWindow.xRxSegments );
+    vListInitialise( &xWindow.xTxSegments );
+
+    /* Below loop fills in various segments in the Rx/Tx list of the window. */
+    for( int i = 0; i < temp; i++ )
+    {
+        /********************** Fill in Rx segments ********************/
+        xRxSegmentListItem[ i ].xSegmentItem.pvOwner = &( xRxSegmentListItem[ i ] );
+
+        /* Make the container of the queue item is NULL. */
+        xRxSegmentListItem[ i ].xQueueItem.pxContainer = NULL;
+
+        __CPROVER_file_local_FreeRTOS_TCP_WIN_c_vListInsertGeneric( &xWindow.xRxSegments,
+                                                                    &( xRxSegmentListItem[ i ].xSegmentItem ), &xWindow.xRxSegments.xListEnd );
+
+
+        /********************** Fill in Tx segments ********************/
+        xTxSegmentListItem[ i ].xSegmentItem.pvOwner = &( xTxSegmentListItem[ i ] );
+
+        /* Make the container of the queue item is NULL. */
+        xTxSegmentListItem[ i ].xQueueItem.pxContainer = NULL;
+
+        __CPROVER_file_local_FreeRTOS_TCP_WIN_c_vListInsertGeneric( &xWindow.xTxSegments,
+                                                                    &( xTxSegmentListItem[ i ].xSegmentItem ), &xWindow.xTxSegments.xListEnd );
+    }
+
+    /* Call the function. The function is internally called from just one location
+     * where it is made sure that the parameter passed to the function is non-NULL.
+     * Therefore, a non-NULL value is passed to the function. */
+    vTCPWindowDestroy( &xWindow );
+}
