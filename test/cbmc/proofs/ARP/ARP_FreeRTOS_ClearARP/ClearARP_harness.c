dd6659c0684bf60a374f6e056f5202ecc1fb87f3
Aniruddha Kanhere <kanherea@amazon.com>
2021-01-13 21:33:41 -0800

    Update ClearARP proof

--- test/cbmc/proofs/ARP/ARP_FreeRTOS_ClearARP/ClearARP_harness.c
+++ test/cbmc/proofs/ARP/ARP_FreeRTOS_ClearARP/ClearARP_harness.c
@@ -1,3 +1,6 @@
+/* CBMC includes */
+#include "cbmc.h"
+
 /* FreeRTOS includes. */
 #include "FreeRTOS.h"
 #include "queue.h"
@@ -5,8 +8,12 @@
 /* FreeRTOS+TCP includes. */
 #include "FreeRTOS_IP.h"
 #include "FreeRTOS_ARP.h"
+#include "FreeRTOS_Routing.h"
 
 void harness()
 {
-    FreeRTOS_ClearARP();
+    /* Randomly assign a pointer to be sent to the function. */
+    struct xNetworkEndPoint *pxEndPoint = safeMalloc( sizeof( struct xNetworkEndPoint ) );
+
+    FreeRTOS_ClearARP( pxEndPoint );
 }
