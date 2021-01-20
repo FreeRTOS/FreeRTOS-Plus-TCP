fbaaa215883c5db8afc51b5a98f1ffc9bf40f95e
Aniruddha Kanhere <60444055+AniruddhaKanhere@users.noreply.github.com>
2021-01-20 20:35:30 +0000

    Update the ARPProcessPacket proof

--- test/cbmc/proofs/ARP/ARPProcessPacket/ARPProcessPacket_harness.c
+++ test/cbmc/proofs/ARP/ARPProcessPacket/ARPProcessPacket_harness.c
@@ -6,10 +6,58 @@
 #include "FreeRTOS_IP.h"
 #include "FreeRTOS_IP_Private.h"
 #include "FreeRTOS_ARP.h"
+#include "FreeRTOS_Routing.h"
+
+/* Global variables accessible throughout the program. Used in adding
+ * Network interface/endpoint. */
+NetworkInterface_t xNetworkInterface1;
+NetworkEndPoint_t xEndPoint1;
+
+const uint8_t ucIPAddress2[ 4 ];
+const uint8_t ucNetMask2[ 4 ];
+const uint8_t ucGatewayAddress2[ 4 ];
+const uint8_t ucDNSServerAddress2[ 4 ];
+const uint8_t ucMACAddress[ 6 ];
 
 void harness()
 {
+    NetworkBufferDescriptor_t xLocalNetworkBufferDescriptor;
     ARPPacket_t xARPFrame;
+    NetworkEndPoint_t xLocalEndPoint;
 
-    eARPProcessPacket( &xARPFrame );
+    xLocalNetworkBufferDescriptor.pucEthernetBuffer = &xARPFrame;
+
+    if( nondet_bool() )
+    {
+        xLocalNetworkBufferDescriptor.pxEndPoint = NULL;
+    }
+    else
+    {
+        xLocalNetworkBufferDescriptor.pxEndPoint = &xLocalEndPoint;
+    }
+
+    /* Assume that the list of interfaces/endpoints is not initialized. */
+    __CPROVER_assume( pxNetworkInterfaces == NULL );
+    __CPROVER_assume( pxNetworkEndPoints == NULL );
+
+    /* Non-deterministically add a network-interface and its endpoint. */
+    if( nondet_bool() )
+    {
+        /* Add the network interfaces to the list. */
+        FreeRTOS_AddNetworkInterface( &xNetworkInterface1 );
+
+        if( nondet_bool() )
+        {
+            /* Fill the endpoints and put them in the network interface. */
+            FreeRTOS_FillEndPoint( &xNetworkInterface1,
+                                   &xEndPoint1,
+                                   ucIPAddress2,
+                                   ucNetMask2,
+                                   ucGatewayAddress2,
+                                   ucDNSServerAddress2,
+                                   ucMACAddress );
+        }
+    }
+
+    eARPProcessPacket( &xLocalNetworkBufferDescriptor );
 }
