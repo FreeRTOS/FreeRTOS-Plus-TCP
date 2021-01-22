d997993706648d8cf5f815cb7e917be32bc38a04
Aniruddha Kanhere <60444055+AniruddhaKanhere@users.noreply.github.com>
2021-01-22 20:27:53 +0000

    Proof update

--- test/cbmc/proofs/DNS/DNSgetHostByName/DNSgetHostByName_harness.c
+++ test/cbmc/proofs/DNS/DNSgetHostByName/DNSgetHostByName_harness.c
@@ -14,6 +14,7 @@
 #include "FreeRTOS_UDP_IP.h"
 #include "FreeRTOS_DNS.h"
 #include "FreeRTOS_DHCP.h"
+#include "FreeRTOS_Routing.h"
 #include "NetworkBufferManagement.h"
 #include "NetworkInterface.h"
 
@@ -36,6 +37,48 @@
 * MAX_HOSTNAME_LEN.  We have to bound this length because we have to
 * bound the iterations of strcmp.
 ****************************************************************/
+
+NetworkBufferDescriptor_t * pxGetNetworkBufferWithDescriptor( size_t xRequestedSizeBytes,
+                                                              TickType_t xBlockTimeTicks )
+{
+    __CPROVER_assert(
+        xRequestedSizeBytes + ipBUFFER_PADDING < CBMC_MAX_OBJECT_SIZE,
+        "pxGetNetworkBufferWithDescriptor: request too big" );
+
+   NetworkBufferDescriptor_t * pxReturn = nondet_bool() ? NULL : malloc( sizeof( * pxReturn ) );
+
+   if( pxReturn != NULL )
+   {
+       pxReturn->pucEthernetBuffer = malloc( xRequestedSizeBytes + ipBUFFER_PADDING );
+
+       if( pxReturn->pucEthernetBuffer != NULL )
+       {
+           pxReturn->xDataLength = xRequestedSizeBytes;
+           pxReturn->pucEthernetBuffer += ipBUFFER_PADDING;
+       }
+       else
+       {
+           free(pxReturn);
+           pxReturn = NULL;
+       }
+   }
+
+   return pxReturn;
+}
+
+void vReleaseNetworkBufferAndDescriptor( NetworkBufferDescriptor_t * const pxNetworkBuffer )
+{
+    __CPROVER_assert( pxNetworkBuffer != NULL,
+                      "Precondition: pxNetworkBuffer != NULL" );
+
+    if( pxNetworkBuffer->pucEthernetBuffer != NULL )
+    {
+        pxNetworkBuffer->pucEthernetBuffer -= ipBUFFER_PADDING;
+        free( pxNetworkBuffer->pucEthernetBuffer );
+    }
+
+    free( pxNetworkBuffer );
+}
 
 /****************************************************************
 * Abstract prvParseDNSReply proved memory save in ParseDNSReply.
@@ -78,12 +121,26 @@ size_t __CPROVER_file_local_FreeRTOS_DNS_c_prvCreateDNSMessage( uint8_t * pucUDP
     return size;
 }
 
+
+/** A list of all network end-points.  Each element has a next pointer. */
+extern struct xNetworkEndPoint * pxNetworkEndPoints;
+
+/** A list of all network interfaces: */
+extern struct xNetworkInterface * pxNetworkInterfaces;
+
 void harness()
 {
     size_t len;
 
     __CPROVER_assume( len <= MAX_HOSTNAME_LEN );
     char * pcHostName = safeMalloc( len );
+
+    /* Assume that the list of interfaces/endpoints is not initialized.
+     * Note: These variables are defined in FreeRTOS_Routing.c in global scope.
+     *       They serve as a list to the network interfaces and the corresponding
+     *       endpoints respectively. And are defined as NULL initially. */
+    __CPROVER_assume( pxNetworkInterfaces == NULL );
+    __CPROVER_assume( pxNetworkEndPoints == NULL );
 
     __CPROVER_assume( len > 0 ); /* prvProcessDNSCache strcmp */
     __CPROVER_assume( pcHostName != NULL );
