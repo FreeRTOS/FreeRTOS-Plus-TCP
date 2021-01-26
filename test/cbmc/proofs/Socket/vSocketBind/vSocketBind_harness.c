a95c8d8bc4ac2fffbe3fef12465f379b3863b05a
Aniruddha Kanhere <60444055+AniruddhaKanhere@users.noreply.github.com>
2021-01-26 18:58:37 +0000

    Update proof and consolidate the configurations

---
+++ test/cbmc/proofs/Socket/vSocketBind/vSocketBind_harness.c
@@ -0,0 +1,80 @@
+/* Standard includes. */
+#include <stdint.h>
+#include <stdio.h>
+
+/* FreeRTOS includes. */
+#include "FreeRTOS.h"
+#include "list.h"
+#include "task.h"
+
+/* FreeRTOS+TCP includes. */
+#include "FreeRTOS_IP.h"
+#include "FreeRTOS_IP_Private.h"
+#include "FreeRTOS_Sockets.h"
+#include "FreeRTOS_Routing.h"
+
+#include "memory_assignments.c"
+
+uint16_t prvGetPrivatePortNumber( BaseType_t xProtocol );
+
+
+uint16_t prvGetPrivatePortNumber( BaseType_t xProtocol )
+{
+    uint16_t usResult;
+
+    return usResult;
+}
+
+BaseType_t xIPIsNetworkTaskReady( void )
+{
+    /* Return true saying that the task is ready. */
+    return pdTRUE;
+}
+
+
+/* Random number generator provided by the application. In our case, CBMC provides
+ * an indeterministic value. */
+BaseType_t xApplicationGetRandomNumber( uint32_t * pulNumber )
+{
+    __CPROVER_assert( pulNumber != NULL, "Argument to xApplicationGetRandomNumber cannot be NULL" );
+
+    if( nondet_bool() )
+    {
+        *pulNumber = nondet_uint32_t();
+        return 1;
+    }
+    else
+    {
+        *pulNumber = NULL;
+        return 0;
+    }
+}
+
+void harness()
+{
+    FreeRTOS_Socket_t * pxSocket = ensure_FreeRTOS_Socket_t_is_allocated();
+
+    __CPROVER_assume( pxSocket != NULL );
+    __CPROVER_assume( pxSocket != FREERTOS_INVALID_SOCKET );
+
+    pxSocket->pxEndPoint = safeMalloc( sizeof( *(pxSocket->pxEndPoint) ) );
+    __CPROVER_assume( pxSocket->pxEndPoint != NULL );
+
+    struct freertos_sockaddr * pxBindAddress = safeMalloc( sizeof( struct freertos_sockaddr ) );
+
+    #if( ipconfigALLOW_SOCKET_SEND_WITHOUT_BIND == 0 )
+        /* The library asserts that pxBindAddress cannot be
+         * NULL if the above configuration is set. */
+        __CPROVER_assume( pxBindAddress != NULL );
+    #endif
+
+    /* uxAddressLength is not used in this implementation. */
+    size_t uxAddressLength;
+
+    BaseType_t xInternal;
+
+    /* Call to init the socket list. */
+    vNetworkSocketsInit();
+
+    vSocketBind( pxSocket, pxBindAddress, uxAddressLength, xInternal );
+}
