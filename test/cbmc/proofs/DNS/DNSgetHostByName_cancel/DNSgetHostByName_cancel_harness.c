38989cfdd44772500593ed2e87e92aefcd20595c
Aniruddha Kanhere <60444055+AniruddhaKanhere@users.noreply.github.com>
2021-01-26 01:08:16 +0000

    Update Proof

--- test/cbmc/proofs/DNS/DNSgetHostByName_cancel/DNSgetHostByName_cancel_harness.c
+++ test/cbmc/proofs/DNS/DNSgetHostByName_cancel/DNSgetHostByName_cancel_harness.c
@@ -1,3 +1,5 @@
+#include "cbmc.h"
+
 /* FreeRTOS includes. */
 #include "FreeRTOS.h"
 #include "queue.h"
@@ -5,41 +7,44 @@
 
 /* FreeRTOS+TCP includes. */
 #include "FreeRTOS_IP.h"
+#include "FreeRTOS_Sockets.h"
 #include "FreeRTOS_DNS.h"
 #include "FreeRTOS_IP_Private.h"
 
 
 /* This proof assumes the length of pcHostName is bounded by MAX_HOSTNAME_LEN. This also abstracts the concurrency. */
-
 void vDNSInitialise( void );
 
-void vDNSSetCallBack( const char * pcHostName,
-                      void * pvSearchID,
-                      FOnDNSEvent pCallbackFunction,
-                      TickType_t xTimeout,
-                      TickType_t xIdentifier );
+void __CPROVER_file_local_FreeRTOS_DNS_c_vDNSSetCallBack( const char * pcHostName,
+                                                          void * pvSearchID,
+                                                          FOnDNSEvent pCallbackFunction,
+                                                          TickType_t xTimeout,
+                                                          TickType_t xIdentifier );
 
-void * safeMalloc( size_t xWantedSize ) /* Returns a NULL pointer if the wanted size is 0. */
-{
-    if( xWantedSize == 0 )
-    {
-        return NULL;
-    }
-
-    uint8_t byte;
-
-    return byte ? malloc( xWantedSize ) : NULL;
-}
 
 /* Abstraction of xTaskCheckForTimeOut from task pool. This also abstracts the concurrency. */
 BaseType_t xTaskCheckForTimeOut( TimeOut_t * const pxTimeOut,
                                  TickType_t * const pxTicksToWait )
 {
+    BaseType_t xReturn;
+
+    /* Return an arbitrary value. */
+    return xReturn;
 }
 
 /* Abstraction of xTaskResumeAll from task pool. This also abstracts the concurrency. */
 BaseType_t xTaskResumeAll( void )
 {
+    BaseType_t xReturn;
+
+    /* Return an arbitrary value. */
+    return xReturn;
+}
+
+/* Abstraction of vTaskSuspendAll from task pool. This also abstracts the concurrency. */
+void vTaskSuspendAll( void )
+{
+    return;
 }
 
 /* The function func mimics the callback function.*/
@@ -47,25 +52,36 @@ void func( const char * pcHostName,
            void * pvSearchID,
            uint32_t ulIPAddress )
 {
+    return;
 }
 
 void harness()
 {
-    vDNSInitialise(); /* We initialize the callbacklist in order to be able to check for functions that timed out. */
     size_t pvSearchID;
     FOnDNSEvent pCallback = func;
     TickType_t xTimeout;
     TickType_t xIdentifier;
     size_t len;
 
-    __CPROVER_assume( len >= 0 && len <= MAX_HOSTNAME_LEN );
-    char * pcHostName = safeMalloc( len );
+    /* len is a size_t variable and hence always positive. */
+    __CPROVER_assume( ( len > 0 ) && ( len <= MAX_HOSTNAME_LEN ) );
 
-    if( len && pcHostName )
+    char * pcHostName = nondet_bool ? malloc( len ) : NULL;
+    __CPROVER_assume( pcHostName != NULL );
+
+    if( pcHostName != NULL )
     {
         pcHostName[ len - 1 ] = NULL;
     }
 
-    vDNSSetCallBack( pcHostName, &pvSearchID, pCallback, xTimeout, xIdentifier ); /* Add an item to be able to check the cancel function if the list is non-empty. */
-    FreeRTOS_gethostbyname_cancel( &pvSearchID );
+    /* We initialize the callbacklist in order to be able to check for functions that timed out. */
+    vDNSInitialise();
+
+    if( nondet_bool() )
+    {
+        /* Add an item to be able to check the cancel function if the list is non-empty. */
+        __CPROVER_file_local_FreeRTOS_DNS_c_vDNSSetCallBack( pcHostName, &pvSearchID, pCallback, xTimeout, xIdentifier );
+    }
+
+    FreeRTOS_gethostbyname_cancel( nondet_bool() ? &pvSearchID : NULL );
 }
