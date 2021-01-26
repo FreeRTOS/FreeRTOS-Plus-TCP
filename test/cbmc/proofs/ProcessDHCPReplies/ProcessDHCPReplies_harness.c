215bfade52fdf5772a355bb353699ad3b795512f
Aniruddha Kanhere <60444055+AniruddhaKanhere@users.noreply.github.com>
2021-01-26 19:41:32 +0000

    Update the comments

--- test/cbmc/proofs/ProcessDHCPReplies/ProcessDHCPReplies_harness.c
+++ test/cbmc/proofs/ProcessDHCPReplies/ProcessDHCPReplies_harness.c
@@ -17,15 +17,17 @@
 
 /****************************************************************
 * Signature of function under test
+* The function name (prvProcessDHCPReplies) is mangled due to the
+* use of a CBMC flag (--export-file-local-symbols) which allows us
+* to access this static function outside the source file in which
+* it is defined.
 ****************************************************************/
-
 BaseType_t __CPROVER_file_local_FreeRTOS_DHCP_c_prvProcessDHCPReplies( BaseType_t xExpectedMessageType,
                                                                        NetworkEndPoint_t * pxEndPoint );
 
 /****************************************************************
 * The proof for FreeRTOS_gethostbyname.
 ****************************************************************/
-
 void harness()
 {
     /* Omitting model of an unconstrained xDHCPData because xDHCPData is */
@@ -34,6 +36,10 @@ void harness()
 
     BaseType_t xExpectedMessageType;
     NetworkEndPoint_t xEndPoint;
+
+    /* The function under test is a private (static) function called only by
+     * the TCP stack. The stack asserts that the endpoint cannot be NULL
+     * before the function under test is invoked. */
     NetworkEndPoint_t * pxEndPoint = &xEndPoint;
 
     __CPROVER_file_local_FreeRTOS_DHCP_c_prvProcessDHCPReplies( xExpectedMessageType, pxEndPoint );
