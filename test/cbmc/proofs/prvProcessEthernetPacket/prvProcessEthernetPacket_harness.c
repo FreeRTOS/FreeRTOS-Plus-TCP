78692210a4c84ea0ffefddc19411035a60d3c7e0
Aniruddha Kanhere <60444055+AniruddhaKanhere@users.noreply.github.com>
2021-01-28 07:00:04 +0000

    Update proof

--- test/cbmc/proofs/prvProcessEthernetPacket/prvProcessEthernetPacket_harness.c
+++ test/cbmc/proofs/prvProcessEthernetPacket/prvProcessEthernetPacket_harness.c
@@ -17,6 +17,7 @@
 #include "NetworkInterface.h"
 #include "NetworkBufferManagement.h"
 #include "FreeRTOS_ARP.h"
+#include "FreeRTOS_Routing.h"
 
 #include "memory_assignments.c"
 #include "freertos_api.c"
@@ -56,9 +57,78 @@ eFrameProcessingResult_t __CPROVER_file_local_FreeRTOS_IP_c_prvProcessIPPacket( 
     return result;
 }
 
+
+/* Network Interface Output function is a portable low level function
+ * which actually sends the data. We have assumed a correct
+ * implementation of this function in this proof. */
+BaseType_t NetworkInterfaceOutput( struct xNetworkInterface * pxDescriptor,
+                                   NetworkBufferDescriptor_t * const pxNetworkBuffer,
+                                   BaseType_t xReleaseAfterSend )
+{
+    BaseType_t xReturn;
+
+    /* Assert some basic conditions. */
+    __CPROVER_assert( pxDescriptor, "Descriptor cannot be NULL" );
+    __CPROVER_assert( pxNetworkBuffer, "NetworkBuffer cannot be NULL" );
+
+    /* Return an arbitrary value. */
+    return xReturn;
+}
+
+/* Global variables accessible throughout the program. Used in adding
+ * Network interface/endpoint. */
+NetworkInterface_t xNetworkInterface1;
+NetworkEndPoint_t xEndPoint1;
+
+const uint8_t ucIPAddress2[ 4 ];
+const uint8_t ucNetMask2[ 4 ];
+const uint8_t ucGatewayAddress2[ 4 ];
+const uint8_t ucDNSServerAddress2[ 4 ];
+const uint8_t ucMACAddress[ 6 ];
+
 void harness()
 {
+    /* Assume that the list of interfaces/endpoints is not initialized.
+     * These are defined in the FreeRTOS_Routing.c file and used as a
+     * list by the TCP stack. */
+    __CPROVER_assume( pxNetworkInterfaces == NULL );
+    __CPROVER_assume( pxNetworkEndPoints == NULL );
+
+    /* Non-deterministically add a network-interface. */
+    if( nondet_bool() )
+    {
+        /* Add the network interfaces to the list. */
+        FreeRTOS_AddNetworkInterface( &xNetworkInterface1 );
+
+        /* Fill the endpoints and put them in the network interface. */
+        FreeRTOS_FillEndPoint( &xNetworkInterface1,
+                               &xEndPoint1,
+                               ucIPAddress2,
+                               ucNetMask2,
+                               ucGatewayAddress2,
+                               ucDNSServerAddress2,
+                               ucMACAddress );
+
+        /* Add the output function to the endpoint. It will be called
+         * if this endpoint is found. */
+        xEndPoint1.pxNetworkInterface->pfOutput = NetworkInterfaceOutput;
+    }
+
+    /* Define and allocate members of an endpoint to be used later. */
+    struct xNetworkEndPoint xLocalEndPoint;
+
+    xLocalEndPoint.pxNetworkInterface = nondet_bool() ? NULL : malloc( sizeof( *( xLocalEndPoint.pxNetworkInterface ) ) );
+
+    /* The network interface of the endpoint cannot be NULL. */
+    __CPROVER_assume( xLocalEndPoint.pxNetworkInterface != NULL );
+
+    /* The output function should also not be NULL */
+    xLocalEndPoint.pxNetworkInterface->pfOutput = NetworkInterfaceOutput;
+
     NetworkBufferDescriptor_t * const pxNetworkBuffer = pxGetNetworkBufferWithDescriptor( ipTOTAL_ETHERNET_FRAME_SIZE, 0 );
+
+    __CPROVER_assume( pxNetworkBuffer != NULL );
+    pxNetworkBuffer->pxEndPoint = nondet_bool() ? NULL : &xLocalEndPoint;
 
     /* The network buffer cannot be NULL for this function call. If it is, it will hit an assert in the function. */
     __CPROVER_assume( pxNetworkBuffer != NULL );
