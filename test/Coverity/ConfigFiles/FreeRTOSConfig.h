53ad3eb66e0b2f3847e2215c78e9a6e68dda3a66
Aniruddha Kanhere <kanherea@amazon.com>
2021-02-18 11:21:21 -0800

    Uncrustify

--- test/Coverity/ConfigFiles/FreeRTOSConfig.h
+++ test/Coverity/ConfigFiles/FreeRTOSConfig.h
@@ -40,38 +40,38 @@
 * http://www.freertos.org/a00110.html
 *----------------------------------------------------------*/
 
-#define configUSE_PREEMPTION                             1
-#define configUSE_PORT_OPTIMISED_TASK_SELECTION          1
-#define configUSE_IDLE_HOOK                              1
-#define configUSE_TICK_HOOK                              1
-#define configUSE_DAEMON_TASK_STARTUP_HOOK               1
-#define configTICK_RATE_HZ                               ( 1000 )                  /* In this non-real time simulated environment the tick frequency has to be at least a multiple of the Win32 tick frequency, and therefore very slow. */
-#define configMINIMAL_STACK_SIZE                         ( ( unsigned short ) 70 ) /* In this simulated case, the stack only has to hold one small structure as the real stack is part of the win32 thread. */
-#define configTOTAL_HEAP_SIZE                            ( ( size_t ) ( 52 * 1024 ) )
-#define configMAX_TASK_NAME_LEN                          ( 12 )
-#define configUSE_TRACE_FACILITY                         1
-#define configUSE_16_BIT_TICKS                           0
-#define configIDLE_SHOULD_YIELD                          1
-#define configUSE_MUTEXES                                1
-#define configCHECK_FOR_STACK_OVERFLOW                   0
-#define configUSE_RECURSIVE_MUTEXES                      1
-#define configQUEUE_REGISTRY_SIZE                        20
-#define configUSE_MALLOC_FAILED_HOOK                     1
-#define configUSE_APPLICATION_TASK_TAG                   1
-#define configUSE_COUNTING_SEMAPHORES                    1
-#define configUSE_ALTERNATIVE_API                        0
-#define configUSE_QUEUE_SETS                             1
-#define configUSE_TASK_NOTIFICATIONS                     1
-#define configSUPPORT_STATIC_ALLOCATION                  1
-#define configINITIAL_TICK_COUNT                         ( ( TickType_t ) 0 )
+#define configUSE_PREEMPTION                       1
+#define configUSE_PORT_OPTIMISED_TASK_SELECTION    1
+#define configUSE_IDLE_HOOK                        1
+#define configUSE_TICK_HOOK                        1
+#define configUSE_DAEMON_TASK_STARTUP_HOOK         1
+#define configTICK_RATE_HZ                         ( 1000 )                        /* In this non-real time simulated environment the tick frequency has to be at least a multiple of the Win32 tick frequency, and therefore very slow. */
+#define configMINIMAL_STACK_SIZE                   ( ( unsigned short ) 70 )       /* In this simulated case, the stack only has to hold one small structure as the real stack is part of the win32 thread. */
+#define configTOTAL_HEAP_SIZE                      ( ( size_t ) ( 52 * 1024 ) )
+#define configMAX_TASK_NAME_LEN                    ( 12 )
+#define configUSE_TRACE_FACILITY                   1
+#define configUSE_16_BIT_TICKS                     0
+#define configIDLE_SHOULD_YIELD                    1
+#define configUSE_MUTEXES                          1
+#define configCHECK_FOR_STACK_OVERFLOW             0
+#define configUSE_RECURSIVE_MUTEXES                1
+#define configQUEUE_REGISTRY_SIZE                  20
+#define configUSE_MALLOC_FAILED_HOOK               1
+#define configUSE_APPLICATION_TASK_TAG             1
+#define configUSE_COUNTING_SEMAPHORES              1
+#define configUSE_ALTERNATIVE_API                  0
+#define configUSE_QUEUE_SETS                       1
+#define configUSE_TASK_NOTIFICATIONS               1
+#define configSUPPORT_STATIC_ALLOCATION            1
+#define configINITIAL_TICK_COUNT                   ( ( TickType_t ) 0 )
 
 /* Software timer related configuration options. */
-#define configUSE_TIMERS                                 1
-#define configTIMER_TASK_PRIORITY                        ( configMAX_PRIORITIES - 1 )
-#define configTIMER_QUEUE_LENGTH                         20
-#define configTIMER_TASK_STACK_DEPTH                     ( configMINIMAL_STACK_SIZE * 2 )
+#define configUSE_TIMERS                           1
+#define configTIMER_TASK_PRIORITY                  ( configMAX_PRIORITIES - 1 )
+#define configTIMER_QUEUE_LENGTH                   20
+#define configTIMER_TASK_STACK_DEPTH               ( configMINIMAL_STACK_SIZE * 2 )
 
-#define configMAX_PRIORITIES                             ( 7 )
+#define configMAX_PRIORITIES                       ( 7 )
 
 /* Run time stats gathering configuration options. */
 unsigned long ulGetRunTimeCounterValue( void ); /* Prototype of function that returns run time counter. */
