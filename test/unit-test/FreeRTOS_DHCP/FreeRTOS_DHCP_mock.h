8574bd7ba520010e6c576ec773f38147ab21e0b2
Aniruddha Kanhere <kanherea@amazon.com>
2021-03-24 17:28:48 -0700

    Uncrustified and ci checks fixed

--- test/unit-test/FreeRTOS_DHCP/FreeRTOS_DHCP_mock.h
+++ test/unit-test/FreeRTOS_DHCP/FreeRTOS_DHCP_mock.h
@@ -1,3 +1,4 @@
 #include "FreeRTOS_DHCP.h"
 
-eDHCPCallbackAnswer_t xApplicationDHCPHook(eDHCPCallbackPhase_t eDHCPPhase, uint32_t ulIPAddress);
+eDHCPCallbackAnswer_t xApplicationDHCPHook( eDHCPCallbackPhase_t eDHCPPhase,
+                                            uint32_t ulIPAddress );
