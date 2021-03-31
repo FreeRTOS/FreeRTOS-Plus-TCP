b2181774b151ed573f0f72034783e6ff2568b9cc
Alfred Gedeon <alfred2g@hotmail.com>
2021-03-31 12:32:05 -0700

    Style: uncritify

--- portable/NetworkInterface/MPS2_AN385/CMSIS/cmsis_version.h
+++ portable/NetworkInterface/MPS2_AN385/CMSIS/cmsis_version.h
@@ -37,5 +37,5 @@
     #define __CM_CMSIS_VERSION_SUB     ( 4U )                               /*!< [15:0]  CMSIS Core(M) sub version */
     #define __CM_CMSIS_VERSION             \
     ( ( __CM_CMSIS_VERSION_MAIN << 16U ) | \
-      __CM_CMSIS_VERSION_SUB )                                              /*!< CMSIS Core(M) version number */
+      __CM_CMSIS_VERSION_SUB ) /*!< CMSIS Core(M) version number */
 #endif
