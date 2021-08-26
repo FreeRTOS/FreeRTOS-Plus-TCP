ca51c5a95ce0ffceabf215f114da4b354e640ece
Aniruddha Kanhere <60444055+AniruddhaKanhere@users.noreply.github.com>
2021-08-25 17:25:57 -0700

    IPv6/multi: Ignore link level only broadcast (#310)
    * Ignore link level broadcasts
    
    * uncrustified
    
    * Use destination instead of source
    
    * Added 'U's to constants
    
    * Uncrustified and fix one cbmc proof
    
    * Update to fix doxygen update changes
    
    Co-authored-by: Cobus van Eeden <35851496+cobusve@users.noreply.github.com>

--- FreeRTOS_IP.c
+++ FreeRTOS_IP.c
@@ -2646,6 +2646,15 @@ static eFrameProcessingResult_t prvAllowIPPacketIPv4( const IPPacket_t * const p
                 /* Packet is not for this node, release it */
                 eReturn = eReleaseBuffer;
             }
+            else if( ( memcmp( ( void * ) xBroadcastMACAddress.ucBytes,
+                               ( void * ) ( pxIPPacket->xEthernetHeader.xDestinationAddress.ucBytes ),
+                               sizeof( MACAddress_t ) ) == 0 ) &&
+                     ( ( FreeRTOS_ntohl( ulDestinationIPAddress ) & 0xffU ) != 0xffU ) )
+            {
+                /* Ethernet address is a broadcast address, but the IP address is not a
+                 * broadcast address. */
+                eReturn = eReleaseBuffer;
+            }
             else
             {
                 /* Packet is not fragmented, destination is this device. */
