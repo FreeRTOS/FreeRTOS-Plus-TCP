445c4c15adb1615197ed8e8c2f4b4d0719dbcb90
Aniruddha Kanhere <kanherea@amazon.com>
2021-09-10 16:50:10 -0700

    Update version number and copyright information

--- test/unit-test/ConfigFiles/IPTraceMacroDefaults.h
+++ test/unit-test/ConfigFiles/IPTraceMacroDefaults.h
@@ -1,6 +1,6 @@
 /*
- * FreeRTOS+TCP V2.2.0
- * Copyright (C) 2020 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
+ * FreeRTOS+TCP V2.3.3
+ * Copyright (C) 2021 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
  * this software and associated documentation files (the "Software"), to deal in
