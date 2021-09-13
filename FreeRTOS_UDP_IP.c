88b145bb084f233119f61aca7c08cea344dcc135
Aniruddha Kanhere <60444055+AniruddhaKanhere@users.noreply.github.com>
2021-09-13 16:36:02 -0700

    Send ARP requests for unknown IP address requests. (#318)
    * Respond to unknown IP address requests with ARP
    
    * Uncrustified
    
    * Fix breaking CBMC proof
    
    * Instead of dropping packets, process them later
    
    * Uncrustified
    
    * Add a timer for ARP response timeout
    
    * Uncrustified
    
    * Add timer declaration
    
    * Check for NULL before freeing network buffer
    
    * Update after @htibosch's comments
    
    * Extended functionality for UDP
    
    * Fix checks and maintain data hiding
    
    * Add param to declaration and to CBMC proof
    
    * Spell check and move function to ARP
    
    * Added trace macros and fixed comments
    
    * Fix CBMC proofs
    
    * Uncrustified

--- FreeRTOS_UDP_IP.c
+++ FreeRTOS_UDP_IP.c
@@ -672,11 +672,14 @@ static BaseType_t prvHandleUDPPacketWithoutSocket( NetworkBufferDescriptor_t * p
  *
  * @param[in] pxNetworkBuffer: The network buffer carrying the UDP packet.
  * @param[in] usPort: The port number on which this packet was received.
+ * @param[out] xIsWaitingARPResolution: If the packet is awaiting ARP resolution, this
+ *                                    pointer will be set to pdTRUE. pdFALSE otherwise.
  *
  * @return pdPASS in case the UDP packet could be processed. Else pdFAIL is returned.
  */
 BaseType_t xProcessReceivedUDPPacket( NetworkBufferDescriptor_t * pxNetworkBuffer,
-                                      uint16_t usPort )
+                                      uint16_t usPort,
+                                      BaseType_t * xIsWaitingARPResolution )
 {
     BaseType_t xReturn = pdPASS;
     FreeRTOS_Socket_t * pxSocket;
@@ -687,6 +690,8 @@ BaseType_t xProcessReceivedUDPPacket( NetworkBufferDescriptor_t * pxNetworkBuffe
     #if ( ipconfigUSE_IPv6 != 0 )
         UDPPacket_IPv6_t * pxUDPPacket_IPv6;
     #endif
+
+    *xIsWaitingARPResolution = pdFALSE;
 
     /* Caller must check for minimum packet size. */
     pxSocket = pxUDPSocketLookup( usPort );
@@ -721,7 +726,20 @@ BaseType_t xProcessReceivedUDPPacket( NetworkBufferDescriptor_t * pxNetworkBuffe
             else
         #endif
         {
-            vARPRefreshCacheEntry( &( pxUDPPacket->xEthernetHeader.xSourceAddress ), pxUDPPacket->xIPHeader.ulSourceIPAddress, pxNetworkBuffer->pxEndPoint );
+            if( xCheckRequiresARPResolution( pxNetworkBuffer ) == pdTRUE )
+            {
+                /* Mark this packet as waiting for ARP resolution. */
+                *xIsWaitingARPResolution = pdTRUE;
+
+                /* Return a fail to show that the frame will not be processed right now. */
+                xReturn = pdFAIL;
+                break;
+            }
+            else
+            {
+                /* IP address is not on the same subnet, ARP table can be updated. */
+                vARPRefreshCacheEntry( &( pxUDPPacket->xEthernetHeader.xSourceAddress ), pxUDPPacket->xIPHeader.ulSourceIPAddress, pxNetworkBuffer->pxEndPoint );
+            }
         }
 
         #if ( ipconfigUSE_CALLBACKS == 1 )
