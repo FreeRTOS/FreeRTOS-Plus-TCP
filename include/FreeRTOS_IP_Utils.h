8bc67744f542cd9f42e36dc983de37a297f6f80b
Aniruddha Kanhere <kanherea@amazon.com>
2021-11-25 13:35:07 -0800

    Fix spell check and add doxygen comment

--- include/FreeRTOS_IP_Utils.h
+++ include/FreeRTOS_IP_Utils.h
@@ -78,7 +78,7 @@ void vPreCheckConfigs( void );
 #if ( ipconfigHAS_PRINTF != 0 )
     extern void vPrintResourceStats( void );
 #else
-    #define vPrintResourceStats()    do {} while( ipFALSE_BOOL )
+    #define vPrintResourceStats()    do {} while( ipFALSE_BOOL )   /**< ipconfigHAS_PRINTF is not defined. Define vPrintResourceStats to a do-while( 0 ). */
 #endif
 
 /*
