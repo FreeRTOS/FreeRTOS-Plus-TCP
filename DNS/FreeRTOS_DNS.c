327cafa4937ae7a8bcda81984df86f69ff39455a
Alfred Gedeon <alfred2g@hotmail.com>
2021-12-20 17:56:31 -0800

    Fix license

--- DNS/FreeRTOS_DNS.c
+++ DNS/FreeRTOS_DNS.c
@@ -1,6 +1,6 @@
 /*
- * FreeRTOS+TCP V2.3.2
- * Copyright (C) 2020 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
+ * FreeRTOS+TCP V2.3.4
+ * Copyright (C) 2021 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
  * this software and associated documentation files (the "Software"), to deal in
@@ -19,8 +19,8 @@
  * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  *
- * https://github.com/FreeRTOS
- * https://www.FreeRTOS.org
+ * http://aws.amazon.com/freertos
+ * http://www.FreeRTOS.org
  */
 
 /**
--- test/unit-test/DNS_Callback/FreeRTOS_UDP_IP_stubs.c
+++ test/unit-test/DNS_Callback/FreeRTOS_UDP_IP_stubs.c
@@ -1,5 +1,5 @@
 /*
- * FreeRTOS+TCP V2.3.3
+ * FreeRTOS+TCP V2.3.4
  * Copyright (C) 2021 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
@@ -19,10 +19,9 @@
  * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  *
- * https://www.FreeRTOS.org
- * https://github.com/FreeRTOS
+ * http://aws.amazon.com/freertos
+ * http://www.FreeRTOS.org
  */
-
 
 /* Include Unity header */
 #include <unity.h>
