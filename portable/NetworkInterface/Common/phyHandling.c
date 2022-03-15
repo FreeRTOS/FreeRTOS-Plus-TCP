9ee43d6ba9f2dd3769a872352e82a83d24a1642b
Ryan Estep <ryan.estep@crown.com>
2022-03-15 15:13:55 +1300

    Override handling of 0x1F MIIM register for KSZ8795
    - KSZ8795 has slightly different mappings for its port operational mode
    after autonegotiation has finished.

--- portable/NetworkInterface/Common/phyHandling.c
+++ portable/NetworkInterface/Common/phyHandling.c
@@ -670,6 +670,34 @@ BaseType_t xPhyStartAutoNegotiation( EthernetPhy_t * pxPhyObject,
                         break;
                 }
             }
+            else if ( ulPhyID == PHY_ID_KSZ8795 )
+            {
+                /* KSZ8795 has a different mapping for the Port Operation Mode Indication field 
+                 *   in the phyREG_1F_PHYSPCS than other similar PHYs:
+                 *     010 = 10BASE-T half-duplex
+                 *     101 = 10BASE-T full-duplex
+                 *     011 = 100BASE-TX half-duplex
+                 *     110 = 100BASE-TX full-duplex
+                 */
+                uint32_t ulControlStatus = 0u;
+                uint32_t ulPortOperationMode = 0u;
+                pxPhyObject->fnPhyRead( xPhyAddress, phyREG_1F_PHYSPCS, &ulControlStatus );
+                ulPortOperationMode = (ulControlStatus >> 8u) & 0x07u;
+
+                ulRegValue = 0;
+
+                /* Detect 10baseT operation */
+                if ( (0x02u == ulPortOperationMode) || (0x05u == ulPortOperationMode) )
+                {
+                    ulRegValue |= phyPHYSTS_SPEED_STATUS;
+                }
+
+                /* Detect full duplex operation */
+                if ( (0x05u == ulPortOperationMode) || (0x06u == ulPortOperationMode) )
+                {
+                    ulRegValue |= phyPHYSTS_DUPLEX_STATUS;
+                }
+            }
             else if( xHas_1F_PHYSPCS( ulPhyID ) )
             {
                 /* 31 RW PHY Special Control Status */
