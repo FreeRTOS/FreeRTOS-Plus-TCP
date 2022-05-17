5777a9b00b5e601daeaf744f17697ecfb1dcb993
Aniruddha Kanhere <60444055+AniruddhaKanhere@users.noreply.github.com>
2022-05-17 15:16:32 -0700

    Fix some bugs and formatting in script (#470)
    * Update the script with suggested changes
    
    * Update GenerateOriginalFiles.py
    
    Co-authored-by: Muneeb Ahmed <54290492+muneebahmed10@users.noreply.github.com>
    
    Co-authored-by: Muneeb Ahmed <54290492+muneebahmed10@users.noreply.github.com>
    Co-authored-by: Cobus van Eeden <35851496+cobusve@users.noreply.github.com>

--- GenerateOriginalFiles.py
+++ GenerateOriginalFiles.py
@@ -1,8 +1,10 @@
-# Prerequisits: Python 3 or greater.
+#!/usr/bin/env python3
+
+# Prerequisits: Python 3.7 or greater.
 #
 # To run this script, go to the root of the FreeRTOS+TCP directory; that is the
 # directory in which this script is present. And then invoke it with:
-# `python3 GenerateOriginalFiles.py`
+# `python3 your/path/to/GenerateOriginalFiles.py`
 #
 # After running this script, you will have 9 source files in the root directory
 # and two additional folders namely `include` and `portable``.
@@ -10,7 +12,9 @@
 
 # Import the shutil library to aid in copying of files and folders to different
 # location.
+import os
 import shutil
+import sys
 
 # FreeRTOS Kernel includes. DO NOT change the order in the list.
 FreeRTOS_Kernel_Includes = [ 'FreeRTOS.h',
@@ -51,7 +55,7 @@ FreeRTOS_TCP_Includes = [ 'FreeRTOS_IP.h',
                           'NetworkBufferManagement.h' ]
 
 # A dictionary to hold the modules to combine and the destination.
-Dictionary = dict()
+MODULES_DICT = dict()
 
 # DO NOT MODIFY. The modules to combine to make up the original FreeRTOS_ARP.c
 ARP_modules_to_combine = [ 'source/FreeRTOS_ARP.c' ]
@@ -93,15 +97,15 @@ TCP_WIN_modules_to_combine = [ 'source/FreeRTOS_TCP_WIN.c',
 UDP_modules_to_combine = [ 'source/FreeRTOS_UDP_IP.c' ]
 
 # Add all the modules and the destinations to the dictionary.
-Dictionary['FreeRTOS_ARP.c'] = ARP_modules_to_combine
-Dictionary['FreeRTOS_DHCP.c'] = DHCP_modules_to_combine
-Dictionary['FreeRTOS_DNS.c'] = DNS_modules_to_combine
-Dictionary['FreeRTOS_IP.c'] = IP_modules_to_combine
-Dictionary['FreeRTOS_Sockets.c'] = Socket_modules_to_combine
-Dictionary['FreeRTOS_Stream_Buffer.c'] = StreamBuffer_modules_to_combine
-Dictionary['FreeRTOS_TCP_IP.c'] = TCP_modules_to_combine
-Dictionary['FreeRTOS_TCP_WIN.c'] = TCP_WIN_modules_to_combine
-Dictionary['FreeRTOS_UDP_IP.c'] = UDP_modules_to_combine
+MODULES_DICT['FreeRTOS_ARP.c'] = ARP_modules_to_combine
+MODULES_DICT['FreeRTOS_DHCP.c'] = DHCP_modules_to_combine
+MODULES_DICT['FreeRTOS_DNS.c'] = DNS_modules_to_combine
+MODULES_DICT['FreeRTOS_IP.c'] = IP_modules_to_combine
+MODULES_DICT['FreeRTOS_Sockets.c'] = Socket_modules_to_combine
+MODULES_DICT['FreeRTOS_Stream_Buffer.c'] = StreamBuffer_modules_to_combine
+MODULES_DICT['FreeRTOS_TCP_IP.c'] = TCP_modules_to_combine
+MODULES_DICT['FreeRTOS_TCP_WIN.c'] = TCP_WIN_modules_to_combine
+MODULES_DICT['FreeRTOS_UDP_IP.c'] = UDP_modules_to_combine
 
 # Sorting function used to add Kernel includes. This is required as the includes
 # need to adhere to a specific order of inclusion.
@@ -114,8 +118,8 @@ def TCPSortingFunction(element):
     return FreeRTOS_TCP_Includes.index(element)
 
 # Get all the includes in all the modules to combine first. This function does
-# not remove duplicates neither does it sort the includes in any specific order.
-def GetIncludeList( ModulesToCombine ):
+# remove the duplicates but does not sort the includes in any specific order.
+def GetIncludeList(ModulesToCombine):
     # This list is used for the FreeRTOS+TCP include files.
     TCP_include_list = list()
     # This list is used for FreeRTOS Kernel include files.
@@ -127,12 +131,12 @@ def GetIncludeList( ModulesToCombine ):
         f = open(filename, "r")
         for line in f.readlines():
             if line.lstrip().startswith('#include '):
-                if ( '<' in line ) and ( line not in StdLib_include_list ):
+                if '<' in line:
                     #if this is a standard library include
                     start_token = '<'
                     end_token = '>'
                     header = line.split(start_token)[1].split(end_token)[0]
-                    StdLib_include_list.append( header)
+                    StdLib_include_list.append(header)
                 else:
                     start_token = '"'
                     end_token = '"'
@@ -143,33 +147,31 @@ def GetIncludeList( ModulesToCombine ):
                     elif header in FreeRTOS_Kernel_Includes:
                         Kernel_include_list.append(header)
                     else:
-                        print( "ERROR: Found " + header + " which is not in any list!" )
+                        print("ERROR: Found " + header + " which is not in any list!")
         f.close()
-    return StdLib_include_list, Kernel_include_list, TCP_include_list
+    return list(set(StdLib_include_list)), list(set(Kernel_include_list)), list(set(TCP_include_list))
 
-# Add the includes in a specific order to the destination file.
-def AddIncludesInFile( modules_to_combine, file_descriptor ):
-    StdLib_include_list, Kernel_include_list, TCP_include_list = GetIncludeList( modules_to_combine )
+# Write the includes in a specific order to the destination file.
+def AddIncludesInFile(modules_to_combine, file_descriptor):
+    StdLib_include_list_unique, Kernel_include_list_unique, TCP_include_list_unique = GetIncludeList(modules_to_combine)
 
-    StdLib_include_list_unique = list( set( StdLib_include_list ) )
     for include in StdLib_include_list_unique:
-        file_descriptor.write( f'#include <{include}>\n' )
+        file_descriptor.write(f'#include <{include}>\n')
 
-    Kernel_include_list_unique = list( set( Kernel_include_list ) )
-    Kernel_include_list_unique.sort( key=KernelSortingFunction )
+    # Sort the Kernel includes in a specific order using KernelSortingFunction.
+    Kernel_include_list_unique.sort(key=KernelSortingFunction)
     for include in Kernel_include_list_unique:
-        file_descriptor.write( f'#include "{include}"\n' )
+        file_descriptor.write(f'#include "{include}"\n')
 
-    TCP_include_list_unique = list( set( TCP_include_list ) )
-    TCP_include_list_unique.sort( key=TCPSortingFunction )
+    # Sort the TCP includes in a specific order using TCPSortingFunction.
+    TCP_include_list_unique.sort(key=TCPSortingFunction)
     for include in TCP_include_list_unique:
-        file_descriptor.write( f'#include "{include}"\n' )
+        file_descriptor.write(f'#include "{include}"\n')
 
 # Function to add the copyright notice to the destination file.
-def AddCopyRightNotice( CopyRightNotice, file_descriptor ):
+def AddCopyRightNotice(CopyRightNotice, file_descriptor):
     for line in CopyRightNotice:
-        file_descriptor.write( f'{line}' )
-    None
+        file_descriptor.write(f'{line}')
 
 # Function to generate the original modules by adding the copyright notice,
 # includes and source to the destination modules.
@@ -179,14 +181,14 @@ def GenerateOriginalModules():
     # Create a list to hold the multi-line copyright notice.
     CopyRightNotice = list()
 
-    for module in Dictionary:
+    for module in MODULES_DICT:
         # Store the copyright notice for future use.
         if FirstIteration:
             FirstIteration = False
             # Open the first module. All modules have copyright notices in them.
-            with open( Dictionary[module][0] ) as f:
+            with open(MODULES_DICT[module][0]) as f:
                 for line in f.readlines():
-                    CopyRightNotice.append( line )
+                    CopyRightNotice.append(line)
 
                     if '*/' in line:
                         # Found the end of commented copyright notice. Break out
@@ -195,19 +197,19 @@ def GenerateOriginalModules():
                 f.close()
 
         # Combine modules only if they were split into multiple ones.
-        if len(Dictionary[module]) is not 1:
-            with open( module, 'w' ) as file_to_write:
+        if len(MODULES_DICT[module]) != 1:
+            with open(module, 'w') as file_to_write:
                 # Add the copyright notice to the destination module.
-                AddCopyRightNotice( CopyRightNotice, file_to_write )
+                AddCopyRightNotice(CopyRightNotice, file_to_write)
 
                 # Add the include files in the destination module.
-                AddIncludesInFile( Dictionary[module], file_to_write )
+                AddIncludesInFile(MODULES_DICT[module], file_to_write)
 
                 # Add the source from all the modules into the destination
                 # module.
-                for filename in Dictionary[module]:
+                for filename in MODULES_DICT[module]:
                     ready_to_write = False
-                    with open( filename, "r") as f:
+                    with open(filename, "r") as f:
                         for line in f.readlines():
                             if ready_to_write:
                                 file_to_write.write(f'{line}')
@@ -219,7 +221,7 @@ def GenerateOriginalModules():
         # Some modules are intact and there is no need to combine.
         # Just copy them to the root directory.
         else:
-            shutil.copy2( Dictionary[module][0] ,module )
+            shutil.copy2(MODULES_DICT[module][0] ,module)
 
 # Function to copy the portable and include directories into the root of the
 # folder.
@@ -230,5 +232,8 @@ def CopyIncludeAndPortableDirs():
     shutil.copytree('source/portable', 'portable')
 
 if __name__ == "__main__":
+    # Change the working directory to the root of repository.
+    os.chdir(os.path.dirname(sys.argv[0]))
+
     GenerateOriginalModules()
     CopyIncludeAndPortableDirs()
