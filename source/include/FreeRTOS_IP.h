29f2c2781361f640646864667a4bf7be838be393
Aniruddha Kanhere <60444055+AniruddhaKanhere@users.noreply.github.com>
2022-07-06 10:05:51 -0700

    Update version number macros

--- source/include/FreeRTOS_IP.h
+++ source/include/FreeRTOS_IP.h
@@ -42,10 +42,11 @@
 
 /* Constants defining the current version of the FreeRTOS+TCP
  * network stack. */
-    #define ipFR_TCP_VERSION_NUMBER    "<DEVELOPMENT BRANCH>"
+    #define ipFR_TCP_VERSION_NUMBER    "2.3.999"
     #define ipFR_TCP_VERSION_MAJOR     2
     #define ipFR_TCP_VERSION_MINOR     3
-    #define ipFR_TCP_VERSION_BUILD     4
+/* Development builds are always version 999. */
+    #define ipFR_TCP_VERSION_BUILD     999
 
 /* Some constants defining the sizes of several parts of a packet.
  * These defines come before including the configuration header files. */
