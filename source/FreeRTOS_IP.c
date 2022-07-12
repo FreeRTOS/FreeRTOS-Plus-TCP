92df402866df0d03b7a014c8dfe39d326db9e357
Alfred Gedeon <alfred2g@hotmail.com>
2022-07-12 10:51:52 -0700

    Add justification for misra 11.6

--- source/FreeRTOS_IP.c
+++ source/FreeRTOS_IP.c
@@ -385,7 +385,8 @@ static void prvProcessIPEventsAndTimers( void )
                     uintptr_t uxState;
                     eDHCPState_t eState;
 
-                    /* Cast in two steps to please MISRA. */
+                    /* uintptr_t is guranteed by the implementation to fit a
+                     * pointer size of the platform */
                     /* coverity[misra_c_2012_rule_11_6_violation] */
                     uxState = ( uintptr_t ) xReceivedEvent.pvData;
                     eState = ( eDHCPState_t ) uxState;
