c7f188747e7f78af02e22ad7f9472f3f6b0db347
Aniruddha Kanhere <60444055+AniruddhaKanhere@users.noreply.github.com>
2022-07-20 14:44:11 -0700

    Added rule 11.8

--- source/FreeRTOS_Sockets.c
+++ source/FreeRTOS_Sockets.c
@@ -1947,7 +1947,7 @@ BaseType_t FreeRTOS_setsockopt( Socket_t xSocket,
                         /* The type cast of the pointer expression "A" to
                          * type "B" removes const qualifier from the pointed to type. */
 
-                        /* MISRA Ref 47 */
+                        /* MISRA Ref 11.8.1 [Function pointer and use of const pointer] */
                         /* coverity[misra_c_2012_rule_11_8_violation] */
                         pxSocket->pxUserWakeCallback = ( SocketWakeupCallback_t ) pvOptionValue;
                         xReturn = 0;
