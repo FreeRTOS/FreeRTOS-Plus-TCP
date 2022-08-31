a69fb406691df586ea765744c110168408cfd54b
Hein Tibosch <hein@htibosch.net>
2022-08-31 15:42:50 +0800

    Applied formatting again

--- source/FreeRTOS_TCP_State_Handling.c
+++ source/FreeRTOS_TCP_State_Handling.c
@@ -201,7 +201,7 @@
                             case eSYN_RECEIVED: /* 4 (server) waiting for a confirming connection request */
 
                                 /* Go back into list mode. Set the TCP status to 'eCLOSED',
-								 * otherwise FreeRTOS_listen() will refuse the action. */
+                                 * otherwise FreeRTOS_listen() will refuse the action. */
                                 pxSocket->u.xTCP.eTCPState = eCLOSED;
                                 ( void ) FreeRTOS_listen( ( Socket_t ) pxSocket, pxSocket->u.xTCP.usBacklog );
                                 xHandled = pdTRUE;
