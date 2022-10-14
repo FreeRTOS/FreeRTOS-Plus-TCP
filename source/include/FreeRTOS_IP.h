e4e236a6c1df6b2d95fc71c6c597e1df89dd0cd6
Aniruddha Kanhere <60444055+AniruddhaKanhere@users.noreply.github.com>
2022-10-13 18:41:14 -0700

    Update mainline to reflect changes after the release. (#563)
    * Update README.md
    
    * Update History.txt
    
    * Update version number macros
    
    * Update manifest.yml

--- source/include/FreeRTOS_IP.h
+++ source/include/FreeRTOS_IP.h
@@ -44,9 +44,9 @@
 
 /* Constants defining the current version of the FreeRTOS+TCP
  * network stack. */
-#define ipFR_TCP_VERSION_NUMBER    "V3.0.999"
+#define ipFR_TCP_VERSION_NUMBER    "V3.1.999"
 #define ipFR_TCP_VERSION_MAJOR     3
-#define ipFR_TCP_VERSION_MINOR     0
+#define ipFR_TCP_VERSION_MINOR     1
 /* Development builds are always version 999. */
 #define ipFR_TCP_VERSION_BUILD     999
 
