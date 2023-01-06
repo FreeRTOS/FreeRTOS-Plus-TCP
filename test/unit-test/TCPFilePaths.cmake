b3d41fb58e1c7c7d23f56b4dcb7be978cc58b74b
Tony Josi <117763118+tony-josi-aws@users.noreply.github.com>
2023-01-06 12:28:34 +0530

    Fix TCP IP failing unit test cases (#649)
    * WIP fix failing test cases for tcp ip utests
    
    * Fixed TCP IP failing unit test cases
    
    * Fixed failing unit tests for TCP IP diffconfig unit tests

--- test/unit-test/TCPFilePaths.cmake
+++ test/unit-test/TCPFilePaths.cmake
@@ -22,6 +22,7 @@ set( TCP_SOURCES
      "${CMAKE_CURRENT_LIST_DIR}/../../source/FreeRTOS_Sockets.c"
      "${CMAKE_CURRENT_LIST_DIR}/../../source/FreeRTOS_Stream_Buffer.c"
      "${CMAKE_CURRENT_LIST_DIR}/../../source/FreeRTOS_TCP_IP.c"
+     "${CMAKE_CURRENT_LIST_DIR}/../../source/FreeRTOS_TCP_IP_IPV4.c"
      "${CMAKE_CURRENT_LIST_DIR}/../../source/FreeRTOS_TCP_Transmission.c"
      "${CMAKE_CURRENT_LIST_DIR}/../../source/FreeRTOS_TCP_Reception.c"
      "${CMAKE_CURRENT_LIST_DIR}/../../source/FreeRTOS_TCP_State_Handling.c"
