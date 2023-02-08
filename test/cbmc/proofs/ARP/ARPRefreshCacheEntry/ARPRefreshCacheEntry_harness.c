759182e00d674f2baaa6856612ffef5085e8310c
tony-josi-aws <tonyjosi@amazon.com>
2023-02-08 04:38:54 +0000

    Fixed proofs for ARPRefreshCacheEntry

--- test/cbmc/proofs/ARP/ARPRefreshCacheEntry/ARPRefreshCacheEntry_harness.c
+++ test/cbmc/proofs/ARP/ARPRefreshCacheEntry/ARPRefreshCacheEntry_harness.c
@@ -11,6 +11,19 @@ void harness()
     MACAddress_t xMACAddress;
     uint32_t ulIPAddress;
 
-    vARPRefreshCacheEntry( &xMACAddress, ulIPAddress );
-    vARPRefreshCacheEntry( NULL, ulIPAddress );
+    /*
+    For this proof, its assumed that the endpoints and interfaces are correctly
+    initialised and the pointers are set correctly.
+    Assumes one endpoints and interface is present.
+    */
+
+    pxNetworkEndPoints = ( NetworkEndPoint_t * ) malloc( sizeof( NetworkEndPoint_t ) );
+    __CPROVER_assume( pxNetworkEndPoints != NULL );
+    __CPROVER_assume( pxNetworkEndPoints->pxNext == NULL );
+
+    NetworkEndPoint_t * pxEndPoint = ( NetworkEndPoint_t * ) malloc( sizeof( NetworkEndPoint_t ) );
+    __CPROVER_assume( pxEndPoint != NULL );
+
+    vARPRefreshCacheEntry( &xMACAddress, ulIPAddress, pxEndPoint );
+    vARPRefreshCacheEntry( NULL, ulIPAddress, pxEndPoint );
 }
