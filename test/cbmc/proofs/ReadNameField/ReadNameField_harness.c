c243eee5c4240766d9ec77f81ea82acd0108104f
tony-josi-aws <tonyjosi@amazon.com>
2023-02-09 17:08:47 +0000

    fixed cbmc proof for DNS_ReadNameField

--- test/cbmc/proofs/ReadNameField/ReadNameField_harness.c
+++ test/cbmc/proofs/ReadNameField/ReadNameField_harness.c
@@ -63,23 +63,20 @@ void harness()
                       "NAME_SIZE >= 4 required for good coverage." );
 
 
-    size_t uxRemainingBytes;
     size_t uxDestLen;
     ParseSet_t pxSet;
 
-    pxSet.pucByte = malloc( uxRemainingBytes );
-    //pxSet.pcRequestedName = malloc( uxDestLen );
+    pxSet.pucByte = malloc( pxSet.uxSourceBytesRemaining );
 
     /* Preconditions */
 
-    __CPROVER_assume( uxRemainingBytes < CBMC_MAX_OBJECT_SIZE );
+    __CPROVER_assume( pxSet.uxSourceBytesRemaining < CBMC_MAX_OBJECT_SIZE );
     __CPROVER_assume( uxDestLen < CBMC_MAX_OBJECT_SIZE );
 
-    __CPROVER_assume( uxRemainingBytes <= NETWORK_BUFFER_SIZE );
+    __CPROVER_assume( pxSet.uxSourceBytesRemaining <= NETWORK_BUFFER_SIZE );
     __CPROVER_assume( uxDestLen <= NAME_SIZE );
 
     __CPROVER_assume( pxSet.pucByte != NULL );
-    //__CPROVER_assume( pxSet.pcRequestedName != NULL );
 
     /* Avoid overflow on uxSourceLen - 1U with uxSourceLen == uxRemainingBytes */
     /*__CPROVER_assume(uxRemainingBytes > 0); */
@@ -94,6 +91,6 @@ void harness()
 
     /* Postconditions */
 
-    __CPROVER_assert( index <= uxDestLen + 1 && index <= uxRemainingBytes,
+    __CPROVER_assert( index <= uxDestLen + 1 && index <= pxSet.uxSourceBytesRemaining,
                       "DNS_ReadNameField : index <= uxDestLen+1" );
 }
