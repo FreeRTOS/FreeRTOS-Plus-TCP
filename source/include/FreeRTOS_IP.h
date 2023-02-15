26899692e18cd72a986609088b045ad432db2c19
kar-rahul-aws <118818625+kar-rahul-aws@users.noreply.github.com>
2023-02-15 08:58:02 +0530

    Update FreeRTOS_IP.h

--- source/include/FreeRTOS_IP.h
+++ source/include/FreeRTOS_IP.h
@@ -50,6 +50,8 @@
 #define ipFR_TCP_VERSION_MINOR     0
 /* Development builds are always version 999. */
 #define ipFR_TCP_VERSION_BUILD     999
+/* Using TCP version to support backward compatibility in the Demo files. */
+#define FREERTOS_PLUS_TCP_VERSION   10
 
 /* Some constants defining the sizes of several parts of a packet.
  * These defines come before including the configuration header files. */
