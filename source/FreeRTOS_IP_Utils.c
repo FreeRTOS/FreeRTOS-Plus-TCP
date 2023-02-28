bdb4dfcd6b69633aa281642f50ebce3b10c2ead9
Monika Singh <moninom@amazon.com>
2023-02-28 07:07:30 +0000

    Update snprintf

--- source/FreeRTOS_IP_Utils.c
+++ source/FreeRTOS_IP_Utils.c
@@ -1396,7 +1396,7 @@ const char * FreeRTOS_strerror_r( BaseType_t xErrnum,
             /* MISRA Ref 21.6.1 [snprintf and logging] */
             /* More details at: https://github.com/FreeRTOS/FreeRTOS-Plus-TCP/blob/main/MISRA.md#rule-216 */
             /* coverity[misra_c_2012_rule_21_6_violation] */
-            ( void ) snprintf( pcBuffer, uxLength, "Errno %lx", xErrnum );
+            ( void ) snprintf( pcBuffer, uxLength, "Errno %d", ( int ) xErrnum );
             pcName = NULL;
             break;
     }
