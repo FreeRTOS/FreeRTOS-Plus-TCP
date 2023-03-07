2550e9cb4bdd2f2d0b69daffc47570f4db25f03a
tony-josi-aws <tonyjosi@amazon.com>
2023-03-07 18:48:25 +0530

    fixing error message printed during test build and disabling coroutines flags used in the config files used for test build

--- test/Coverity/ConfigFiles/FreeRTOSConfig.h
+++ test/Coverity/ConfigFiles/FreeRTOSConfig.h
@@ -81,7 +81,7 @@ void vConfigureTimerForRunTimeStats( void ); /* Prototype of function that initi
 #define portGET_RUN_TIME_COUNTER_VALUE()            ulGetRunTimeCounterValue()
 
 /* Co-routine related configuration options. */
-#define configUSE_CO_ROUTINES                     1
+#define configUSE_CO_ROUTINES                     0
 #define configMAX_CO_ROUTINE_PRIORITIES           ( 2 )
 
 /* This demo makes use of one or more example stats formatting functions.  These
