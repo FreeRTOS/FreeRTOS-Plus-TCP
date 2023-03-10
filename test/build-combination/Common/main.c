0a58c4a6f1b26a91bc9971532a1c2567b04d8b7d
Monika Singh <moninom@amazon.com>
2023-03-10 12:44:48 +0000

    Fix compilation when ipconfigIPv4_BACKWARD_COMPATIBLE = 1

--- test/build-combination/Common/main.c
+++ test/build-combination/Common/main.c
@@ -123,9 +123,12 @@ int main( void )
     return 0;
 }
 /*-----------------------------------------------------------*/
-
-void vApplicationIPNetworkEventHook_Multi( eIPCallbackEvent_t eNetworkEvent,
-                                           struct xNetworkEndPoint * pxEndPoint )
+#if defined( ipconfigIPv4_BACKWARD_COMPATIBLE ) && ( ipconfigIPv4_BACKWARD_COMPATIBLE == 1 )
+    void vApplicationIPNetworkEventHook( eIPCallbackEvent_t eNetworkEvent )
+#else
+    void vApplicationIPNetworkEventHook_Multi( eIPCallbackEvent_t eNetworkEvent,
+                                               struct xNetworkEndPoint * pxEndPoint )
+#endif
 {
     static BaseType_t xTasksAlreadyCreated = pdFALSE;
 
@@ -314,3 +317,11 @@ void vApplicationPingReplyHook( ePingReplyStatus_t eStatus,
 {
     /* Provide a stub for this function. */
 }
+
+#if ( ipconfigUSE_IPv6 != 0 ) && ( ipconfigUSE_DHCPv6 != 0 )
+    /* DHCPv6 needs a time-stamp, seconds after 1970. */
+    uint32_t ulApplicationTimeHook( void )
+    {
+        return time( NULL );
+    }
+#endif
