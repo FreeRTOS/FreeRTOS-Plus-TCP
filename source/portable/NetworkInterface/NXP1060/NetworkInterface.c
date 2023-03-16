962396d8aced68db2ebe86145653dd253e260d64
GitHub Action <action@github.com>
2023-03-16 20:42:57 +0000

    Uncrustify: triggered by comment.

--- source/portable/NetworkInterface/NXP1060/NetworkInterface.c
+++ source/portable/NetworkInterface/NXP1060/NetworkInterface.c
@@ -130,7 +130,7 @@
 
 /* The number of buffer descriptors in ENET TX ring. */
 #ifndef ENET_TXBD_NUM
-    #define ENET_TXBD_NUM            ( 3 )
+    #define ENET_TXBD_NUM    ( 3 )
 #endif
 
 /* Set the timeout values such that the total timeout adds up to 4000ms. */
@@ -261,9 +261,9 @@ BaseType_t xNetworkInterfaceInitialise( void )
                 break;
             }
             else
-			{
-				eEMACState = xEMAC_WaitPHY;
-			}
+            {
+                eEMACState = xEMAC_WaitPHY;
+            }
 
         /* Fall through. */
         case xEMAC_WaitPHY:
@@ -282,7 +282,7 @@ BaseType_t xNetworkInterfaceInitialise( void )
             }
             else
             {
-            	eEMACState = xEMAC_Init;
+                eEMACState = xEMAC_Init;
             }
 
         /* Fall through. */
@@ -334,13 +334,14 @@ BaseType_t xNetworkInterfaceInitialise( void )
 
         /* Fall through. */
         case xEMAC_Ready:
-        	FreeRTOS_printf( ( "Driver ready for use." ) );
+            FreeRTOS_printf( ( "Driver ready for use." ) );
 
-        	/* Kick the task once the driver is ready. */
-        	if( receiveTaskHandle != NULL )
-        	{
-        	    xTaskNotify( receiveTaskHandle, DRIVER_READY, eSetValueWithOverwrite );
-        	}
+            /* Kick the task once the driver is ready. */
+            if( receiveTaskHandle != NULL )
+            {
+                xTaskNotify( receiveTaskHandle, DRIVER_READY, eSetValueWithOverwrite );
+            }
+
             xResult = pdPASS;
 
             break;
@@ -401,41 +402,41 @@ static void prvEMACHandlerTask( void * parameter )
     {
         if( ulTaskNotifyTake( pdTRUE, pdMS_TO_TICKS( 500 ) ) == pdFALSE )
         {
-        	/* No RX packets for a bit so check for a link. */
+            /* No RX packets for a bit so check for a link. */
             const IPStackEvent_t xNetworkEventDown = { .eEventType = eNetworkDownEvent, .pvData = NULL };
 
             do
             {
-				readStatus = PHY_GetLinkStatus( &phyHandle, &LinkUp );
+                readStatus = PHY_GetLinkStatus( &phyHandle, &LinkUp );
 
-				if( readStatus == kStatus_Success )
-				{
-					if( LinkUp == pdFALSE )
-					{
-						/* The link is down. */
-						bGlobalLinkStatus = false;
-						/* We need to setup the PHY again. */
-						eEMACState = xEMAC_SetupPHY;
+                if( readStatus == kStatus_Success )
+                {
+                    if( LinkUp == pdFALSE )
+                    {
+                        /* The link is down. */
+                        bGlobalLinkStatus = false;
+                        /* We need to setup the PHY again. */
+                        eEMACState = xEMAC_SetupPHY;
 
-						FreeRTOS_printf( ( "Link down!" ) );
+                        FreeRTOS_printf( ( "Link down!" ) );
 
-						xSendEventStructToIPTask( &xNetworkEventDown, 0U );
+                        xSendEventStructToIPTask( &xNetworkEventDown, 0U );
 
-						/* Wait for the driver to finish initialization. */
-						uint32_t ulNotificationValue;
-					    do{
-					    	ulNotificationValue = ulTaskNotifyTake( pdTRUE, portMAX_DELAY );
-					    }while( !( ulNotificationValue & DRIVER_READY ) );
+                        /* Wait for the driver to finish initialization. */
+                        uint32_t ulNotificationValue;
 
-
-					}
-					else
-					{
-						/* The link is still up. */
-						bGlobalLinkStatus = true;
-					}
-				}
-            }while( bGlobalLinkStatus == false );
+                        do
+                        {
+                            ulNotificationValue = ulTaskNotifyTake( pdTRUE, portMAX_DELAY );
+                        } while( !( ulNotificationValue & DRIVER_READY ) );
+                    }
+                    else
+                    {
+                        /* The link is still up. */
+                        bGlobalLinkStatus = true;
+                    }
+                }
+            } while( bGlobalLinkStatus == false );
         }
         else
         {
@@ -543,16 +544,16 @@ static void prvProcessFrame( int length )
             #if ( ( ipconfigHAS_DEBUG_PRINTF == 1 ) && defined( FreeRTOS_debug_printf ) )
                 const EthernetHeader_t * pxEthernetHeader;
                 char ucSource[ 18 ];
-				char ucDestination[ 18 ];
+                char ucDestination[ 18 ];
 
-				pxEthernetHeader = ( ( const EthernetHeader_t * ) pxBufferDescriptor->pucEthernetBuffer );
+                pxEthernetHeader = ( ( const EthernetHeader_t * ) pxBufferDescriptor->pucEthernetBuffer );
 
 
-				FreeRTOS_EUI48_ntop( pxEthernetHeader->xSourceAddress.ucBytes, ucSource, 'A', ':' );
-				FreeRTOS_EUI48_ntop( pxEthernetHeader->xDestinationAddress.ucBytes, ucDestination, 'A', ':' );
+                FreeRTOS_EUI48_ntop( pxEthernetHeader->xSourceAddress.ucBytes, ucSource, 'A', ':' );
+                FreeRTOS_EUI48_ntop( pxEthernetHeader->xDestinationAddress.ucBytes, ucDestination, 'A', ':' );
 
-				FreeRTOS_debug_printf( ( "Invalid target MAC: dropping frame from: %s to: %s\n", ucSource, ucDestination ) );
-			#endif
+                FreeRTOS_debug_printf( ( "Invalid target MAC: dropping frame from: %s to: %s\n", ucSource, ucDestination ) );
+            #endif /* if ( ( ipconfigHAS_DEBUG_PRINTF == 1 ) && defined( FreeRTOS_debug_printf ) ) */
             vReleaseNetworkBufferAndDescriptor( pxBufferDescriptor );
             /* Not sure if a trace is required.  The stack did not want this message */
         }
@@ -582,8 +583,8 @@ static status_t xSetupPHY( phy_config_t * pxConfig )
     status_t xStatus;
 
     /* Set the clock frequency. MDIO handle is pointed to by the PHY handle. */
-	mdioHandle.resource.csrClock_Hz = EXAMPLE_CLOCK_FREQ;
-	mdioHandle.resource.base = ( void * ) ENET_BASE;
+    mdioHandle.resource.csrClock_Hz = EXAMPLE_CLOCK_FREQ;
+    mdioHandle.resource.base = ( void * ) ENET_BASE;
 
     /* Set the configuration to auto-negotiate; set the phy to full duplex mode; the phy's address; and the speed. */
     pxConfig->autoNeg = pdTRUE;
@@ -713,10 +714,10 @@ static status_t xEMACInit( phy_speed_t speed,
     configASSERT( duplex == kPHY_FullDuplex );
 
     ethernetifLocal->RxBuffDescrip = &( rxBuffDescrip_0[ 0 ] );
-	ethernetifLocal->TxBuffDescrip = &( txBuffDescrip_0[ 0 ] );
-	ethernetifLocal->RxDataBuff = &( rxDataBuff_0[ 0 ] );
-	ethernetifLocal->TxDataBuff = &( txDataBuff_0[ 0 ] );
-	ethernetifLocal->base = ( void * ) ENET_BASE;
+    ethernetifLocal->TxBuffDescrip = &( txBuffDescrip_0[ 0 ] );
+    ethernetifLocal->RxDataBuff = &( rxDataBuff_0[ 0 ] );
+    ethernetifLocal->TxDataBuff = &( txDataBuff_0[ 0 ] );
+    ethernetifLocal->base = ( void * ) ENET_BASE;
 
     /* prepare the buffer configuration. */
     buffCfg[ 0 ].rxBdNumber = ENET_RXBD_NUM;                  /* Number of RX buffer descriptors. */
