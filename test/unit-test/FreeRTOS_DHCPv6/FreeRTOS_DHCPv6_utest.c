85fb0634004ca6644f4a128d70e4fc589344bc29
ActoryOu <ousc@amazon.com>
2023-04-27 03:10:14 +0000

    Basic DHCPv6 UT framework.

---
+++ test/unit-test/FreeRTOS_DHCPv6/FreeRTOS_DHCPv6_utest.c
@@ -0,0 +1,86 @@
+/*
+ * FreeRTOS+TCP <DEVELOPMENT BRANCH>
+ * Copyright (C) 2022 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * http://aws.amazon.com/freertos
+ * http://www.FreeRTOS.org
+ */
+
+
+/* Include Unity header */
+#include "unity.h"
+
+/* Include standard libraries */
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+
+#include "mock_task.h"
+#include "mock_list.h"
+
+/*#include "FreeRTOS_IP_stubs.c" */
+#include "catch_assert.h"
+
+#include "FreeRTOS_DHCPv6.h"
+#include "FreeRTOS_DHCPv6_stubs.c"
+
+/* ===========================  EXTERN VARIABLES  =========================== */
+
+/* ============================  Unity Fixtures  ============================ */
+
+/*! called before each test case */
+void setUp( void )
+{
+    InitializeUnitTest();
+}
+
+/*! called after each test case */
+void tearDown( void )
+{
+}
+
+/* ==============================  Test Cases  ============================== */
+
+
+/**
+ * @brief test_eGetDHCPv6State_happy_path
+ * Check if eGetDHCPv6State can return DHCP state correctly.
+ *
+ * Test step:
+ *  - Set endpoint's DHCP state.
+ *  - Call eGetDHCPv6State to get state and check if it's correct.
+ */
+void test_eGetDHCPv6State_happy_path()
+{
+    NetworkEndPoint_t xEndPoint;
+    eDHCPState_t eState = eInitialWait, eStateMax = eNotUsingLeasedAddress;
+    eDHCPState_t eReturnState;
+    
+    memset( &xEndPoint, 0, sizeof( NetworkEndPoint_t ) );
+    
+    for( eState = eInitialWait ; eState <= eNotUsingLeasedAddress ; eState++ )
+    {
+        xEndPoint.xDHCPData.eDHCPState = eState;
+        eReturnState = eGetDHCPv6State( &xEndPoint );
+        TEST_ASSERT_EQUAL( eState, eReturnState );
+    }
+}
