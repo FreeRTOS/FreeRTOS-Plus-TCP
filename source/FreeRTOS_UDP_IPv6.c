3ae27c4e8be959a89cac13e1c5c86772874d02fa
tony-josi-aws <tonyjosi@amazon.com>
2023-04-28 15:28:45 +0530

    fix misra and spellings

--- source/FreeRTOS_UDP_IPv6.c
+++ source/FreeRTOS_UDP_IPv6.c
@@ -435,6 +435,7 @@ BaseType_t xProcessReceivedUDPPacket_IPv6( NetworkBufferDescriptor_t * pxNetwork
     /* Returning pdPASS means that the packet was consumed, released. */
     BaseType_t xReturn = pdPASS;
     FreeRTOS_Socket_t * pxSocket;
+    const UDPPacket_IPv6_t * pxUDPPacket_IPv6;
 
     configASSERT( pxNetworkBuffer != NULL );
     configASSERT( pxNetworkBuffer->pucEthernetBuffer != NULL );
@@ -446,7 +447,7 @@ BaseType_t xProcessReceivedUDPPacket_IPv6( NetworkBufferDescriptor_t * pxNetwork
     /* MISRA Ref 11.3.1 [Misaligned access] */
     /* More details at: https://github.com/FreeRTOS/FreeRTOS-Plus-TCP/blob/main/MISRA.md#rule-113 */
     /* coverity[misra_c_2012_rule_11_3_violation] */
-    const UDPPacket_IPv6_t * pxUDPPacket_IPv6 = ( ( UDPPacket_IPv6_t * ) pxNetworkBuffer->pucEthernetBuffer );
+    pxUDPPacket_IPv6 = ( ( UDPPacket_IPv6_t * ) pxNetworkBuffer->pucEthernetBuffer );
 
     /* Caller must check for minimum packet size. */
     pxSocket = pxUDPSocketLookup( usPort );
