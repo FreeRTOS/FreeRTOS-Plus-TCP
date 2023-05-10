0a145eb82bf849c25cf0f0a716b5140b884eba83
ActoryOu <ousc@amazon.com>
2023-05-10 02:36:32 +0000

    Code beautify and remove unnecessary define

--- source/include/FreeRTOS_DHCPv6.h
+++ source/include/FreeRTOS_DHCPv6.h
@@ -99,13 +99,10 @@
 /** @brief Don't allow the lease time to be too short. */
     #define dhcpv6MINIMUM_LEASE_TIME                   ( pdMS_TO_TICKS( 60000U ) ) /* 60 seconds in ticks. */
 
-    #define ipDHCP_CLIENT_PORT                    67U
-    #define ipDHCP_SERVER_PORT                    68U
-
 /** @brief Default v6 DHCP client port. */
-    #define ipDHCPv6_CLIENT_PORT                  546U
+    #define ipDHCPv6_CLIENT_PORT                       546U
     /** @brief Default v6 DHCP server port. */
-    #define ipDHCPv6_SERVER_PORT                  547U
+    #define ipDHCPv6_SERVER_PORT                       547U
 
 /** @brief The ID of a client or a server. */
     typedef struct xClientServerID
