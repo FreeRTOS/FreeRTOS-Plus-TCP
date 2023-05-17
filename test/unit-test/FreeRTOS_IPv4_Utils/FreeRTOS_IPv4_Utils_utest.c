e475d24fe50a34024103a1ff33880e88b1a4ca87
ActoryOu <ousc@amazon.com>
2023-05-17 09:30:45 +0000

    IPv4_Utils full coverage

--- test/unit-test/FreeRTOS_IPv4_Utils/FreeRTOS_IPv4_Utils_utest.c
+++ test/unit-test/FreeRTOS_IPv4_Utils/FreeRTOS_IPv4_Utils_utest.c
@@ -64,10 +64,6 @@
 
 #include "catch_assert.h"
 
-/* =========================== EXTERN VARIABLES =========================== */
-
-/* ============================ Stubs Functions =========================== */
-
 /* ============================== Test Cases ============================== */
 
 /**
@@ -88,4 +84,146 @@ void test_vSetMultiCastIPv4MacAddress( void )
     TEST_ASSERT_EQUAL( ( uint8_t ) ( ( FreeRTOS_ntohl( ulIP ) >> 16 ) & 0x7fU ), xMACAddress.ucBytes[ 3 ] );
     TEST_ASSERT_EQUAL( ( uint8_t ) ( ( FreeRTOS_ntohl( ulIP ) >> 8 ) & 0xffU ), xMACAddress.ucBytes[ 4 ] );
     TEST_ASSERT_EQUAL( ( uint8_t ) ( ( FreeRTOS_ntohl( ulIP ) ) & 0xffU ), xMACAddress.ucBytes[ 5 ] );
+}
+
+/**
+ * @brief test_prvChecksumIPv4Checks_IPLengthLessThanHeaderLength
+ * To validate if prvChecksumIPv4Checks sets ipINVALID_LENGTH in checksum field when
+ * length in IPv4 header is less than VersionHeaderLength.
+ */
+void test_prvChecksumIPv4Checks_IPLengthLessThanHeaderLength()
+{
+    BaseType_t xReturn;
+    IPPacket_t xIPPacket;
+    struct xPacketSummary xSet;
+    uint8_t ucVersionHeaderLength = 20;
+    uint16_t usLength = ucVersionHeaderLength - 1;
+    uint8_t pucEthernetBuffer[ ipconfigTCP_MSS ];
+    size_t uxBufferLength = usLength + ipSIZE_OF_ETH_HEADER;
+
+    memset( &xIPPacket, 0, sizeof( xIPPacket ) );
+    memset( &xSet, 0, sizeof( xSet ) );
+
+    xSet.pxIPPacket = &xIPPacket;
+    xIPPacket.xIPHeader.usLength = FreeRTOS_htons( usLength );
+    xIPPacket.xIPHeader.ucVersionHeaderLength = ( ucVersionHeaderLength >> 2 );
+
+    xReturn = prvChecksumIPv4Checks( pucEthernetBuffer, uxBufferLength, &xSet );
+
+    TEST_ASSERT_EQUAL( 3, xReturn );
+    TEST_ASSERT_EQUAL( ipINVALID_LENGTH, xSet.usChecksum );
+}
+
+/**
+ * @brief test_prvChecksumIPv4Checks_BufferLessIPPacket
+ * To validate if prvChecksumIPv4Checks sets ipINVALID_LENGTH in checksum field when
+ * buffer size is less than IPv4 packet minimum requirement.
+ */
+void test_prvChecksumIPv4Checks_BufferLessIPPacket()
+{
+    BaseType_t xReturn;
+    IPPacket_t xIPPacket;
+    struct xPacketSummary xSet;
+    uint8_t ucVersionHeaderLength = 20;
+    uint16_t usLength = ucVersionHeaderLength;
+    uint8_t pucEthernetBuffer[ ipconfigTCP_MSS ];
+    size_t uxBufferLength = ipSIZE_OF_ETH_HEADER + ipSIZE_OF_IPv4_HEADER - 1;
+
+    memset( &xIPPacket, 0, sizeof( xIPPacket ) );
+    memset( &xSet, 0, sizeof( xSet ) );
+
+    xSet.pxIPPacket = &xIPPacket;
+    xIPPacket.xIPHeader.usLength = FreeRTOS_htons( usLength );
+    xIPPacket.xIPHeader.ucVersionHeaderLength = ( ucVersionHeaderLength >> 2 );
+
+    xReturn = prvChecksumIPv4Checks( pucEthernetBuffer, uxBufferLength, &xSet );
+
+    TEST_ASSERT_EQUAL( 4, xReturn );
+    TEST_ASSERT_EQUAL( ipINVALID_LENGTH, xSet.usChecksum );
+}
+
+/**
+ * @brief test_prvChecksumIPv4Checks_BufferLessIPHeaderLength
+ * To validate if prvChecksumIPv4Checks sets ipINVALID_LENGTH in checksum field when
+ * buffer size is less than IPv4 header length.
+ */
+void test_prvChecksumIPv4Checks_BufferLessIPHeaderLength()
+{
+    BaseType_t xReturn;
+    IPPacket_t xIPPacket;
+    struct xPacketSummary xSet;
+    uint8_t ucVersionHeaderLength = 24;
+    uint16_t usLength = ucVersionHeaderLength;
+    uint8_t pucEthernetBuffer[ ipconfigTCP_MSS ];
+    size_t uxBufferLength = ipSIZE_OF_ETH_HEADER + ucVersionHeaderLength - 1;
+
+    memset( &xIPPacket, 0, sizeof( xIPPacket ) );
+    memset( &xSet, 0, sizeof( xSet ) );
+
+    xSet.pxIPPacket = &xIPPacket;
+    xIPPacket.xIPHeader.usLength = FreeRTOS_htons( usLength );
+    xIPPacket.xIPHeader.ucVersionHeaderLength = ( ucVersionHeaderLength >> 2 );
+
+    xReturn = prvChecksumIPv4Checks( pucEthernetBuffer, uxBufferLength, &xSet );
+
+    TEST_ASSERT_EQUAL( 5, xReturn );
+    TEST_ASSERT_EQUAL( ipINVALID_LENGTH, xSet.usChecksum );
+}
+
+/**
+ * @brief test_prvChecksumIPv4Checks_BufferLessIPPayloadLength
+ * To validate if prvChecksumIPv4Checks sets ipINVALID_LENGTH in checksum field when
+ * buffer size is less than IPv4 header payload length.
+ */
+void test_prvChecksumIPv4Checks_BufferLessIPPayloadLength()
+{
+    BaseType_t xReturn;
+    IPPacket_t xIPPacket;
+    struct xPacketSummary xSet;
+    uint8_t ucVersionHeaderLength = 20;
+    uint16_t usLength = ucVersionHeaderLength + ipSIZE_OF_TCP_HEADER;
+    uint8_t pucEthernetBuffer[ ipconfigTCP_MSS ];
+    size_t uxBufferLength = ipSIZE_OF_ETH_HEADER + usLength - 1;
+
+    memset( &xIPPacket, 0, sizeof( xIPPacket ) );
+    memset( &xSet, 0, sizeof( xSet ) );
+
+    xSet.pxIPPacket = &xIPPacket;
+    xIPPacket.xIPHeader.usLength = FreeRTOS_htons( usLength );
+    xIPPacket.xIPHeader.ucVersionHeaderLength = ( ucVersionHeaderLength >> 2 );
+
+    xReturn = prvChecksumIPv4Checks( pucEthernetBuffer, uxBufferLength, &xSet );
+
+    TEST_ASSERT_EQUAL( 6, xReturn );
+    TEST_ASSERT_EQUAL( ipINVALID_LENGTH, xSet.usChecksum );
+}
+
+/**
+ * @brief test_prvChecksumIPv4Checks_Pass
+ * To validate if prvChecksumIPv4Checks returns 0 when pass.
+ */
+void test_prvChecksumIPv4Checks_Pass()
+{
+    BaseType_t xReturn;
+    IPPacket_t xIPPacket;
+    struct xPacketSummary xSet;
+    uint8_t ucVersionHeaderLength = 20;
+    uint16_t usLength = ucVersionHeaderLength + ipSIZE_OF_TCP_HEADER;
+    uint8_t pucEthernetBuffer[ ipconfigTCP_MSS ];
+    size_t uxBufferLength = ipSIZE_OF_ETH_HEADER + usLength;
+
+    memset( &xIPPacket, 0, sizeof( xIPPacket ) );
+    memset( &xSet, 0, sizeof( xSet ) );
+
+    xSet.pxIPPacket = &xIPPacket;
+    xIPPacket.xIPHeader.usLength = FreeRTOS_htons( usLength );
+    xIPPacket.xIPHeader.ucVersionHeaderLength = ( ucVersionHeaderLength >> 2 );
+    xIPPacket.xIPHeader.ucProtocol = ipPROTOCOL_TCP;
+
+    xReturn = prvChecksumIPv4Checks( pucEthernetBuffer, uxBufferLength, &xSet );
+
+    TEST_ASSERT_EQUAL( 0, xReturn );
+    TEST_ASSERT_EQUAL( ipPROTOCOL_TCP, xSet.ucProtocol );
+    TEST_ASSERT_EQUAL( &pucEthernetBuffer[ ipSIZE_OF_ETH_HEADER + ucVersionHeaderLength ], xSet.pxProtocolHeaders );
+    TEST_ASSERT_EQUAL( ipSIZE_OF_TCP_HEADER, xSet.usProtocolBytes );
 }
