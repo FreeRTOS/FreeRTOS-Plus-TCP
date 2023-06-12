4e99735b09cb36d9b92d2ce3650f2a7b21386f23
Monika Singh <moninom@amazon.com>
2023-06-12 11:16:44 +0000

    Update comment

--- test/cbmc/proofs/ND/prvProcessICMPMessage_IPv6/ProcessICMPMessage_IPv6_harness.c
+++ test/cbmc/proofs/ND/prvProcessICMPMessage_IPv6/ProcessICMPMessage_IPv6_harness.c
@@ -82,7 +82,7 @@ BaseType_t xSendEventStructToIPTask( const IPStackEvent_t * pxEvent,
 }
 
 /* This is an output function implemented by a third party and
- need not be tested with this proof. */
+ * need not be tested with this proof. */
 BaseType_t NetworkInterfaceOutputFunction_Stub( struct xNetworkInterface * pxDescriptor,
                                                 NetworkBufferDescriptor_t * const pxNetworkBuffer,
                                                 BaseType_t xReleaseAfterSend )
@@ -119,17 +119,17 @@ void harness()
 {
     NetworkBufferDescriptor_t * pxNetworkBuffer = ensure_FreeRTOS_NetworkBuffer_is_allocated();
     uint32_t ulLen;
-    NetworkBufferDescriptor_t xLocalBuffer;
     uint16_t usEthernetBufferSize;
+    NetworkBufferDescriptor_t xLocalBuffer;
 
-    /* The code does not expect both of these to be equal to NULL at the same time. */
+    /* The code does not expect pxNetworkBuffer to be NULL. */
     __CPROVER_assume( pxNetworkBuffer != NULL );
 
     __CPROVER_assume( ( ulLen >= sizeof( ICMPPacket_IPv6_t ) ) && ( ulLen < ipconfigNETWORK_MTU ) );
 
     pxNetworkBuffer->xDataLength = ulLen;
     pxNetworkBuffer->pucEthernetBuffer = safeMalloc( ulLen );
-    __CPROVER_assume( pxNetworkBuffer->pucEthernetBuffer );
+    __CPROVER_assume( pxNetworkBuffer->pucEthernetBuffer != NULL );
 
     /* Add an end point to the network buffer present. Its assumed that the
      * network interface layer correctly assigns the end point to the generated buffer. */
@@ -139,25 +139,31 @@ void harness()
      * which is validated only when bIPv6 is set*/
     __CPROVER_assume( ( pxNetworkBuffer->pxEndPoint != NULL ) && ( pxNetworkBuffer->pxEndPoint->bits.bIPv6 == pdTRUE_UNSIGNED ) );
 
+    /* Non deterministically determine whether the pxARPWaitingNetworkBuffer will
+     * point to some valid data or will it be NULL. */
+    if( nondet_bool() )
+    {
+        /* The packet must at least be as big as an IPv6 Packet. The size is not
+         * checked in the function as the pointer is stored by the IP-task itself
+         * and therefore it will always be of the required size. */
+        __CPROVER_assume( usEthernetBufferSize >= sizeof( IPPacket_IPv6_t ) );
 
-    /* Initializing pxARPWaitingNetworkBuffer */
+        /* Add matching data length to the network buffer descriptor. */
+        __CPROVER_assume( xLocalBuffer.xDataLength == usEthernetBufferSize );
 
-    /* The packet must at least be as big as an IPv6 Packet. The size is not
-     * checked in the function as the pointer is stored by the IP-task itself
-     * and therefore it will always be of the required size. */
-    __CPROVER_assume( usEthernetBufferSize >= sizeof( IPPacket_IPv6_t ) );
+        xLocalBuffer.pucEthernetBuffer = malloc( usEthernetBufferSize );
 
-    /* Add matching data length to the network buffer descriptor. */
-    __CPROVER_assume( xLocalBuffer.xDataLength == usEthernetBufferSize );
+        /* Since this pointer is maintained by the IP-task, either the pointer
+         * pxARPWaitingNetworkBuffer will be NULL or xLocalBuffer.pucEthernetBuffer
+         * will be non-NULL. */
+        __CPROVER_assume( xLocalBuffer.pucEthernetBuffer != NULL );
 
-    xLocalBuffer.pucEthernetBuffer = malloc( usEthernetBufferSize );
-
-    /* Since this pointer is maintained by the IP-task, either the pointer
-     * pxARPWaitingNetworkBuffer will be NULL or xLocalBuffer.pucEthernetBuffer
-     * will be non-NULL. */
-    __CPROVER_assume( xLocalBuffer.pucEthernetBuffer != NULL );
-
-    pxARPWaitingNetworkBuffer = &xLocalBuffer;
+        pxARPWaitingNetworkBuffer = &xLocalBuffer;
+    }
+    else
+    {
+        pxARPWaitingNetworkBuffer = NULL;
+    }
 
     prvProcessICMPMessage_IPv6( pxNetworkBuffer );
 }
