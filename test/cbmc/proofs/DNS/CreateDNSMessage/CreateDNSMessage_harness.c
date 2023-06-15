26354e6247d2348b55b0cda6421d268d3908431e
tony-josi-aws <tonyjosi@amazon.com>
2023-06-15 22:35:25 +0530

    fix review comments

--- test/cbmc/proofs/DNS/CreateDNSMessage/CreateDNSMessage_harness.c
+++ test/cbmc/proofs/DNS/CreateDNSMessage/CreateDNSMessage_harness.c
@@ -10,6 +10,11 @@
 
 #include "cbmc.h"
 
+size_t prvCreateDNSMessage( uint8_t * pucUDPPayloadBuffer,
+                            const char * pcHostName,
+                            TickType_t uxIdentifier,
+                            UBaseType_t uxHostType );
+
 void harness()
 {
     size_t uxExpectedPayloadLength;
