65436ba8940b2a3eaf5c050b9ed2479a3a2b177a
tony-josi-aws <tonyjosi@amazon.com>
2023-06-15 09:37:55 +0530

    fix formatting

--- test/cbmc/proofs/DNS/CreateDNSMessage/CreateDNSMessage_harness.c
+++ test/cbmc/proofs/DNS/CreateDNSMessage/CreateDNSMessage_harness.c
@@ -11,13 +11,12 @@
 #include "cbmc.h"
 
 size_t prvCreateDNSMessage( uint8_t * pucUDPPayloadBuffer,
-                                        const char * pcHostName,
-                                        TickType_t uxIdentifier,
-                                        UBaseType_t uxHostType );
+                            const char * pcHostName,
+                            TickType_t uxIdentifier,
+                            UBaseType_t uxHostType );
 
 void harness()
 {
-
     size_t uxExpectedPayloadLength;
     TickType_t uxIdentifier;
     UBaseType_t uxHostType;
@@ -27,23 +26,23 @@ void harness()
     __CPROVER_assume( len > 0 && len <= MAX_HOSTNAME_LEN );
 
     /* pcHostName is tested to be valid prior */
-    char * pcHostName = malloc( len ); 
+    char * pcHostName = malloc( len );
 
     if( len && pcHostName )
     {
         /* Make sure the string ends with a NULL, this is expected */
         pcHostName[ len - 1 ] = NULL;
     }
-    
-    /* prvCreateDNSMessage() is expected to be called with a ethernet buffer of 
-    the following size */
+
+    /* prvCreateDNSMessage() is expected to be called with a ethernet buffer of
+     * the following size */
     uxExpectedPayloadLength = sizeof( DNSMessage_t ) +
-                                strlen( pcHostName ) +
-                                sizeof( uint16_t ) +
-                                sizeof( uint16_t ) + 2U;
+                              strlen( pcHostName ) +
+                              sizeof( uint16_t ) +
+                              sizeof( uint16_t ) + 2U;
 
     /* Add header size */
-    if(nondet_bool())
+    if( nondet_bool() )
     {
         uxExpectedPayloadLength += ipSIZE_OF_ETH_HEADER + ipSIZE_OF_IPv6_HEADER + ipSIZE_OF_UDP_HEADER;
     }
@@ -53,8 +52,7 @@ void harness()
     }
 
     /* pucUDPPayloadBuffer is tested to be valid prior */
-    uint8_t * pucUDPPayloadBuffer = malloc( uxExpectedPayloadLength ); 
-                                
-    prvCreateDNSMessage(pucUDPPayloadBuffer, pcHostName, uxIdentifier, uxHostType);
+    uint8_t * pucUDPPayloadBuffer = malloc( uxExpectedPayloadLength );
 
+    prvCreateDNSMessage( pucUDPPayloadBuffer, pcHostName, uxIdentifier, uxHostType );
 }
