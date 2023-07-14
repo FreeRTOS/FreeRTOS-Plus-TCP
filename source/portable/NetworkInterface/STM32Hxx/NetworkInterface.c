c463e558c0233114896cebc44c671b35b478676a
Tony Josi <tonyjosi@amazon.com>
2023-07-14 11:59:56 +0530

    fix review comments

--- source/portable/NetworkInterface/STM32Hxx/NetworkInterface.c
+++ source/portable/NetworkInterface/STM32Hxx/NetworkInterface.c
@@ -391,6 +391,40 @@ static BaseType_t xSTM32H_NetworkInterfaceInitialise( NetworkInterface_t * pxInt
                     }
                 #endif /* if ( ipconfigUSE_IPv6 != 0 ) */
 
+                switch( pxEndPoint->bits.bIPv6 ) 
+                {
+                    #if ( ipconfigUSE_IPv4 != 0 )
+                        case pdFALSE_UNSIGNED:
+                            if( xEthHandle.Init.MACAddr != ( uint8_t * ) pxEndPoint->xMACAddress.ucBytes )
+                            {
+                                prvMACAddressConfig( &xEthHandle, xMACEntry, pxEndPoint->xMACAddress.ucBytes );
+                                xMACEntry += 8;
+                            }
+                            break;
+                    #endif /* ( ipconfigUSE_IPv4 != 0 ) */
+
+                    #if ( ipconfigUSE_IPv6 != 0 )
+                        case pdTRUE_UNSIGNED:
+                            {
+                                uint8_t ucMACAddress[ 6 ] = { 0x33, 0x33, 0xff, 0, 0, 0 };
+
+                                ucMACAddress[ 3 ] = pxEndPoint->ipv6_settings.xIPAddress.ucBytes[ 13 ];
+                                ucMACAddress[ 4 ] = pxEndPoint->ipv6_settings.xIPAddress.ucBytes[ 14 ];
+                                ucMACAddress[ 5 ] = pxEndPoint->ipv6_settings.xIPAddress.ucBytes[ 15 ];
+
+                                /* Allow traffic destined to Solicited-Node multicast address of this endpoint
+                                * for Duplicate Address Detection (DAD) */
+                                prvMACAddressConfig( &xEthHandle, xMACEntry, ucMACAddress );
+                            xMACEntry += 8;
+                            }
+                            break;
+                    #endif /* ( ipconfigUSE_IPv6 != 0 ) */
+
+                    default:
+                        /* MISRA 16.4 Compliance */
+                        break;
+                }
+
                 if( xMACEntry > ( BaseType_t ) ETH_MAC_ADDRESS3 )
                 {
                     /* No more locations available. */
