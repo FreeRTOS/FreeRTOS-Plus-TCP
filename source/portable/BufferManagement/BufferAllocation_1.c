ecd1307f1e8062b90f04834fe7435edbbf86d67c
Soren Ptak <ptaksoren@gmail.com>
2023-07-20 16:21:57 -0700

    Uncrustify PR Check Update (#969)
    * Update uncrustify to use version other repos use, clean up the way it runs, formatting changes to needed files
    * Moving the lexicon.txt to where it lives in all other repos
    * Removing executable permissions from .c files
    ---------

--- source/portable/BufferManagement/BufferAllocation_1.c
+++ source/portable/BufferManagement/BufferAllocation_1.c
@@ -94,7 +94,7 @@ static void prvShowWarnings( void );
     {
     #define ipconfigBUFFER_ALLOC_UNLOCK_FROM_ISR()               \
     portCLEAR_INTERRUPT_MASK_FROM_ISR( uxSavedInterruptStatus ); \
-    }
+}
 
     #define ipconfigBUFFER_ALLOC_LOCK()      taskENTER_CRITICAL()
     #define ipconfigBUFFER_ALLOC_UNLOCK()    taskEXIT_CRITICAL()
