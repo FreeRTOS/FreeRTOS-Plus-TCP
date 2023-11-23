aae4c304ca0ba409c2adf339e4122547bfbca487
Monika Singh <moninom@amazon.com>
2023-11-23 16:51:15 +0000

    MISRA fix 12.1

--- source/portable/BufferManagement/BufferAllocation_2.c
+++ source/portable/BufferManagement/BufferAllocation_2.c
@@ -77,7 +77,7 @@
     #define ASSERT_CONCAT_( a, b )    a ## b
     #define ASSERT_CONCAT( a, b )     ASSERT_CONCAT_( a, b )
     #define STATIC_ASSERT( e ) \
-    enum { ASSERT_CONCAT( assert_line_, __LINE__ ) = 1 / ( !!( e ) ) }
+    enum { ASSERT_CONCAT( assert_line_, __LINE__ ) = ( 1 / ( !!( e ) ) ) }
 
     STATIC_ASSERT( ipconfigETHERNET_MINIMUM_PACKET_BYTES <= baMINIMAL_BUFFER_SIZE );
 #endif
