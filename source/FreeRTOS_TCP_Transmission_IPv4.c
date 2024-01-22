b92b22b33e34309041aaf48ce81ec461470b3a89
Hein Tibosch <hein_tibosch@yahoo.es>
2024-01-22 17:31:49 +0800

    Do not clear pxEndPoint in TCPReturnPacket()

--- source/FreeRTOS_TCP_Transmission_IPv4.c
+++ source/FreeRTOS_TCP_Transmission_IPv4.c
@@ -99,6 +99,8 @@ void prvTCPReturnPacket_IPV4( FreeRTOS_Socket_t * pxSocket,
     void * pvCopyDest = NULL;
     const size_t uxIPHeaderSize = ipSIZE_OF_IPv4_HEADER;
     uint32_t ulDestinationIPAddress;
+    eARPLookupResult_t eResult;
+    NetworkEndPoint_t * pxEndPoint = NULL;
 
     do
     {
@@ -234,17 +236,22 @@ void prvTCPReturnPacket_IPV4( FreeRTOS_Socket_t * pxSocket,
 
             pvCopySource = &pxEthernetHeader->xSourceAddress;
             ulDestinationIPAddress = pxIPHeader->ulDestinationIPAddress;
-            eARPLookupResult_t eResult;
 
-            eResult = eARPGetCacheEntry( &ulDestinationIPAddress, &xMACAddress, &( pxNetworkBuffer->pxEndPoint ) );
+            eResult = eARPGetCacheEntry( &ulDestinationIPAddress, &xMACAddress, &pxEndPoint );
 
             if( eResult == eARPCacheHit )
             {
                 pvCopySource = &xMACAddress;
+                pxNetworkBuffer->pxEndPoint = pxEndPoint;
             }
             else
             {
                 pvCopySource = &pxEthernetHeader->xSourceAddress;
+            }
+
+            if( pxNetworkBuffer->pxEndPoint == NULL )
+            {
+                break;
             }
 
             /* Fill in the destination MAC addresses. */
@@ -298,10 +305,18 @@ void prvTCPReturnPacket_IPV4( FreeRTOS_Socket_t * pxSocket,
             }
             else
             {
-                /* Nothing to do: the buffer has been passed to DMA and will be released after use */
+                xDoRelease = pdFALSE;
+                /* The buffer has been passed to DMA and will be released after use */
             }
         } /* if( pxNetworkBuffer != NULL ) */
     } while( ipFALSE_BOOL );
+
+    if( xDoRelease == pdTRUE )
+    {
+        vReleaseNetworkBufferAndDescriptor( pxNetworkBuffer );
+    }
+
+    pxNetworkBuffer = NULL;
 }
 /*-----------------------------------------------------------*/
 
