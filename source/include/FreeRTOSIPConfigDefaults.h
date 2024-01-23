53354c2b9453911bb14f7653a522764724d8fdc8
Holden <holden@zenithaerotech.com>
2024-01-23 05:47:51 -0500

    update static assertion definition

--- source/include/FreeRTOSIPConfigDefaults.h
+++ source/include/FreeRTOSIPConfigDefaults.h
@@ -66,18 +66,28 @@
 /*
  * Compile time assertion with zero runtime effects.
  * It will assert on 'e' not being zero, as it tries to divide by it.
- * It will also print the line where the error occurred in case of failure.
  */
-#ifndef static_assert
-    /* MISRA Ref 20.10.1 [Lack of sizeof operator and compile time error checking] */
-    /* More details at: https://github.com/FreeRTOS/FreeRTOS-Plus-TCP/blob/main/MISRA.md#rule-2010 */
-    /* coverity[misra_c_2012_rule_20_10_violation] */
-    #define ASSERT_CONCAT_( a, b )  a ## b
-    #define ASSERT_CONCAT( a, b )   ASSERT_CONCAT_( a, b )
-    #define STATIC_ASSERT( e )  enum { ASSERT_CONCAT( assert_line_, __LINE__ ) = ( 1 / ( !!( e ) ) ) }
+
+#ifdef static_assert
+    #define STATIC_ASSERT( e ) static_assert( e, "FreeRTOS-Plus-TCP Error" )
+#elif defined( _Static_assert )
+    #define STATIC_ASSERT( e ) _Static_assert( e, "FreeRTOS-Plus-TCP Error" )
 #else
-    #define STATIC_ASSERT( e ) static_assert( e, __LINE__ )
+/* MISRA Ref 20.10.1 [Lack of sizeof operator and compile time error checking] */
+/* More details at: https://github.com/FreeRTOS/FreeRTOS-Plus-TCP/blob/main/MISRA.md#rule-2010 */
+/* coverity[misra_c_2012_rule_20_10_violation] */
+    #define ASSERT_CONCAT_( a, b ) a##b
+    #define ASSERT_CONCAT( a, b ) ASSERT_CONCAT_( a, b )
+    #ifdef __COUNTER__
+        #define STATIC_ASSERT( e ) \
+            ;enum { ASSERT_CONCAT( static_assert_, __COUNTER__ ) = 1 / ( int ) ( !!( e ) ) }
+    #else
+        #define STATIC_ASSERT( e ) \
+            ;enum { ASSERT_CONCAT( assert_line_, __LINE__ ) = 1 / ( int ) ( !!( e ) ) }
+    #endif
 #endif
+
+/*---------------------------------------------------------------------------*/
 
 /*
  * Used to define away static keyword for CBMC proofs
