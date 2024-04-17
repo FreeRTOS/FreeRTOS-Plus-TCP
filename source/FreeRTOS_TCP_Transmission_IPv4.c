222f93c44283fa423c942b22724342a1a439d3b1
Andreas Nordal <andreas.nordal@zivid.com>
2024-04-17 14:43:46 +0200

    FreeRTOS_TCP_Transmission_IPv4,6(): Move deref behind nullptr check
    Fix a possible null pointer derefernce:
    The pointer returned by pxDuplicateNetworkBufferWithDescriptor()
    was being dereferenced outside its null checks.
    This could only happen if ipconfigZERO_COPY_TX_DRIVER was enabled.
    
    Conveniently, the statement that used the pointer could be moved
    into the scope of the null check.

--- source/FreeRTOS_TCP_Transmission_IPv4.c
+++ source/FreeRTOS_TCP_Transmission_IPv4.c
@@ -141,16 +141,16 @@ void prvTCPReturnPacket_IPV4( FreeRTOS_Socket_t * pxSocket,
         }
         #endif /* ipconfigZERO_COPY_TX_DRIVER */
 
-        /* MISRA Ref 11.3.1 [Misaligned access] */
-        /* More details at: https://github.com/FreeRTOS/FreeRTOS-Plus-TCP/blob/main/MISRA.md#rule-113 */
-        /* coverity[misra_c_2012_rule_11_3_violation] */
-        pxIPHeader = ( ( IPHeader_t * ) &( pxNetworkBuffer->pucEthernetBuffer[ ipSIZE_OF_ETH_HEADER ] ) );
-
         #ifndef __COVERITY__
             if( pxNetworkBuffer != NULL ) /* LCOV_EXCL_BR_LINE the 2nd branch will never be reached */
         #endif
         {
             NetworkInterface_t * pxInterface;
+
+            /* MISRA Ref 11.3.1 [Misaligned access] */
+            /* More details at: https://github.com/FreeRTOS/FreeRTOS-Plus-TCP/blob/main/MISRA.md#rule-113 */
+            /* coverity[misra_c_2012_rule_11_3_violation] */
+            pxIPHeader = ( ( IPHeader_t * ) &( pxNetworkBuffer->pucEthernetBuffer[ ipSIZE_OF_ETH_HEADER ] ) );
 
             /* Map the Ethernet buffer onto a TCPPacket_t struct for easy access to the fields. */
 
