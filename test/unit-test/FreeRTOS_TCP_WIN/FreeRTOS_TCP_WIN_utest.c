54434341f1ee3564e21b559858a45f8a801f7a8a
tony-josi-aws <tonyjosi@amazon.com>
2024-06-05 16:35:30 +0530

    Fix unit tests

--- test/unit-test/FreeRTOS_TCP_WIN/FreeRTOS_TCP_WIN_utest.c
+++ test/unit-test/FreeRTOS_TCP_WIN/FreeRTOS_TCP_WIN_utest.c
@@ -239,7 +239,7 @@ void test_vTCPWindowDestroy_list_no_segment_container( void )
     vTCPWindowDestroy( &xWindow );
 }
 
-void test_vTCPWindowCreate_success( void )
+void test_xTCPWindowCreate_success( void )
 {
     TCPWindow_t xWindow = { 0 };
     uint32_t ulRxWindowLength = 0;
@@ -247,6 +247,7 @@ void test_vTCPWindowCreate_success( void )
     uint32_t ulAckNumber = 0;
     uint32_t ulSequenceNumber = 0;
     uint32_t ulMSS = 0;
+    BaseType_t xReturn;
 
     void * mlc = malloc( ipconfigTCP_WIN_SEG_COUNT * sizeof( xTCPSegments[ 0 ] ) );
 
@@ -267,16 +268,17 @@ void test_vTCPWindowCreate_success( void )
     vListInitialise_ExpectAnyArgs();
 
 
-    vTCPWindowCreate( &xWindow,
+    xReturn = xTCPWindowCreate( &xWindow,
                       ulRxWindowLength,
                       ulTxWindowLength,
                       ulAckNumber,
                       ulSequenceNumber,
                       ulMSS );
+    TEST_ASSERT_EQUAL( pdPASS, xReturn );
     free( mlc );
 }
 
-void test_vTCPWindowCreate_tcp_segment_null( void )
+void test_xTCPWindowCreate_AllocationFailed( void )
 {
     TCPWindow_t xWindow = { 0 };
     uint32_t ulRxWindowLength = 0;
@@ -284,6 +286,38 @@ void test_vTCPWindowCreate_tcp_segment_null( void )
     uint32_t ulAckNumber = 0;
     uint32_t ulSequenceNumber = 0;
     uint32_t ulMSS = 0;
+    BaseType_t xReturn;
+
+    /* ->prvCreateSectors */
+    vListInitialise_ExpectAnyArgs();
+    pvPortMalloc_ExpectAnyArgsAndReturn( NULL );
+
+    /* back */
+    vListInitialise_ExpectAnyArgs();
+    vListInitialise_ExpectAnyArgs();
+    vListInitialise_ExpectAnyArgs();
+    vListInitialise_ExpectAnyArgs();
+    vListInitialise_ExpectAnyArgs();
+
+
+    xReturn = xTCPWindowCreate( &xWindow,
+                      ulRxWindowLength,
+                      ulTxWindowLength,
+                      ulAckNumber,
+                      ulSequenceNumber,
+                      ulMSS );
+    TEST_ASSERT_EQUAL( pdFAIL, xReturn );
+}
+
+void test_xTCPWindowCreate_tcp_segment_null( void )
+{
+    TCPWindow_t xWindow = { 0 };
+    uint32_t ulRxWindowLength = 0;
+    uint32_t ulTxWindowLength = 0;
+    uint32_t ulAckNumber = 0;
+    uint32_t ulSequenceNumber = 0;
+    uint32_t ulMSS = 0;
+    BaseType_t xReturn;
 
     /* ->prvCreateSectors */
     vListInitialise_ExpectAnyArgs();
@@ -296,15 +330,16 @@ void test_vTCPWindowCreate_tcp_segment_null( void )
     vListInitialise_ExpectAnyArgs();
 
 
-    vTCPWindowCreate( &xWindow,
+    xReturn = xTCPWindowCreate( &xWindow,
                       ulRxWindowLength,
                       ulTxWindowLength,
                       ulAckNumber,
                       ulSequenceNumber,
                       ulMSS );
+    TEST_ASSERT_EQUAL( pdFAIL, xReturn );
 }
 
-void test_vTCPWindowCreate_null_tcpSegment( void )
+void test_xTCPWindowCreate_null_tcpSegment( void )
 {
     TCPWindow_t xWindow = { 0 };
     uint32_t ulRxWindowLength = 0;
@@ -312,6 +347,7 @@ void test_vTCPWindowCreate_null_tcpSegment( void )
     uint32_t ulAckNumber = 0;
     uint32_t ulSequenceNumber = 0;
     uint32_t ulMSS = 0;
+    BaseType_t xReturn;
 
     xTCPSegments = ( TCPSegment_t * ) 32;
 
@@ -321,12 +357,13 @@ void test_vTCPWindowCreate_null_tcpSegment( void )
     vListInitialise_ExpectAnyArgs();
     vListInitialise_ExpectAnyArgs();
 
-    vTCPWindowCreate( &xWindow,
+    xReturn = xTCPWindowCreate( &xWindow,
                       ulRxWindowLength,
                       ulTxWindowLength,
                       ulAckNumber,
                       ulSequenceNumber,
                       ulMSS );
+    TEST_ASSERT_EQUAL( pdPASS, xReturn );
     TEST_ASSERT_EQUAL( ulRxWindowLength, xWindow.xSize.ulRxWindowLength );
     TEST_ASSERT_EQUAL( ulTxWindowLength, xWindow.xSize.ulTxWindowLength );
 }
