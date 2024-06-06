09ebd928809a95623869c217228ea57240313f82
tony-josi-aws <tonyjosi@amazon.com>
2024-06-06 17:21:21 +0530

    Add UTs for new API

--- test/unit-test/FreeRTOS_DNS/FreeRTOS_DNS_utest.c
+++ test/unit-test/FreeRTOS_DNS/FreeRTOS_DNS_utest.c
@@ -2244,3 +2244,35 @@ void test_FreeRTOS_freeaddrinfo_NullInput( void )
 {
     FreeRTOS_freeaddrinfo( NULL );
 }
+
+/**
+ * @brief Preference is IPv6
+ */
+void test_FreeRTOS_SetDNSIPPreference_IPv4( void )
+{
+    BaseType_t xReturn;
+    xReturn = FreeRTOS_SetDNSIPPreference( xPreferenceIPv4 );
+    TEST_ASSERT_EQUAL( pdPASS, xReturn );
+    TEST_ASSERT_EQUAL( xPreferenceIPv4, xDNS_IP_Preference );
+}
+
+/**
+ * @brief Preference is IPv6
+ */
+void test_FreeRTOS_SetDNSIPPreference_IPv6( void )
+{
+    BaseType_t xReturn;
+    xReturn = FreeRTOS_SetDNSIPPreference( xPreferenceIPv6 );
+    TEST_ASSERT_EQUAL( pdPASS, xReturn );
+    TEST_ASSERT_EQUAL( xPreferenceIPv6, xDNS_IP_Preference );
+}
+
+/**
+ * @brief Preference is None
+ */
+void test_FreeRTOS_SetDNSIPPreference_None( void )
+{
+    BaseType_t xReturn;
+    xReturn = FreeRTOS_SetDNSIPPreference( xPreferenceNone );
+    TEST_ASSERT_EQUAL( pdFAIL, xReturn );
+}
