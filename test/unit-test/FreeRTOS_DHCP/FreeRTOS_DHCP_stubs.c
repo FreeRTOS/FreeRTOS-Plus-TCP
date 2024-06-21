0158df5be0013147e80f7270e62be8f1ef1db3aa
tony-josi-aws <tonyjosi@amazon.com>
2024-06-21 12:13:30 +0530

    Fix unit test with latst kernel

--- test/unit-test/FreeRTOS_DHCP/FreeRTOS_DHCP_stubs.c
+++ test/unit-test/FreeRTOS_DHCP/FreeRTOS_DHCP_stubs.c
@@ -133,8 +133,8 @@ void vApplicationDaemonTaskStartupHook( void )
 {
 }
 void vApplicationGetTimerTaskMemory( StaticTask_t ** ppxTimerTaskTCBBuffer,
-                                     StackType_t ** ppxTimerTaskStackBuffer,
-                                     uint32_t * pulTimerTaskStackSize )
+                                         StackType_t ** ppxTimerTaskStackBuffer,
+                                         configSTACK_DEPTH_TYPE * puxTimerTaskStackSize )
 {
 }
 void vPortDeleteThread( void * pvTaskToDelete )
@@ -181,7 +181,7 @@ void vPortCloseRunningThread( void * pvTaskToDelete,
 }
 void vApplicationGetIdleTaskMemory( StaticTask_t ** ppxIdleTaskTCBBuffer,
                                     StackType_t ** ppxIdleTaskStackBuffer,
-                                    uint32_t * pulIdleTaskStackSize )
+                                    configSTACK_DEPTH_TYPE * puxIdleTaskStackSize )
 {
 }
 void vConfigureTimerForRunTimeStats( void )
