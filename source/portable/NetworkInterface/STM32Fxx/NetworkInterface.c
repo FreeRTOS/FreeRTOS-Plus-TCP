6b11b135faa835ecf7e9d197c6284fdddfac879d
tony-josi-aws <tonyjosi@amazon.com>
2024-06-27 15:46:06 +0530

    Update version info to V4.2.1

--- source/portable/NetworkInterface/STM32Fxx/NetworkInterface.c
+++ source/portable/NetworkInterface/STM32Fxx/NetworkInterface.c
@@ -4,7 +4,7 @@
  */
 
 /*
- * FreeRTOS+TCP <DEVELOPMENT BRANCH>
+ * FreeRTOS+TCP V4.2.1
  * Copyright (C) 2022 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
  *
  * SPDX-License-Identifier: MIT
