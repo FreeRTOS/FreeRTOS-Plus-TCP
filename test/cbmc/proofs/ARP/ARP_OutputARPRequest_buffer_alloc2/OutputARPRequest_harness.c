1bb5934e82aaf11f1d36622054184c749a3fed93
ActoryOu <ousc@amazon.com>
2024-07-09 09:19:46 +0000

    Fix ARP_OutputARPRequest_buffer_alloc2 CBMC test

--- test/cbmc/proofs/ARP/ARP_OutputARPRequest_buffer_alloc2/OutputARPRequest_harness.c
+++ test/cbmc/proofs/ARP/ARP_OutputARPRequest_buffer_alloc2/OutputARPRequest_harness.c
@@ -55,6 +55,15 @@ BaseType_t NetworkInterfaceOutputFunction_Stub( struct xNetworkInterface * pxDes
     }
 }
 
+BaseType_t xIsCallingFromIPTask( void )
+{
+    BaseType_t xReturn;
+
+    __CPROVER_assume( xReturn == pdFALSE || xReturn == pdTRUE );
+
+    return xReturn;
+}
+
 void harness()
 {
     BaseType_t xRes = xNetworkBuffersInitialise();
