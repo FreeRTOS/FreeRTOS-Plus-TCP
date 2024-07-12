d50adead1544686508c89bbf523e2d3e36852cc8
Ching-Hsin,Lee <chinglee@amazon.com>
2024-07-12 13:09:53 +0800

    Fix format in header file

--- source/include/FreeRTOS_IP.h
+++ source/include/FreeRTOS_IP.h
@@ -44,11 +44,11 @@
 
 /* Constants defining the current version of the FreeRTOS+TCP
  * network stack. */
-#define ipFR_TCP_VERSION_NUMBER      "V4.2.2"
-#define ipFR_TCP_VERSION_MAJOR       4
-#define ipFR_TCP_VERSION_MINOR       2
+#define ipFR_TCP_VERSION_NUMBER    "V4.2.2"
+#define ipFR_TCP_VERSION_MAJOR     4
+#define ipFR_TCP_VERSION_MINOR     2
 /* Development builds are always version 999. */
-#define ipFR_TCP_VERSION_BUILD       2
+#define ipFR_TCP_VERSION_BUILD     2
 
 /* Some constants defining the sizes of several parts of a packet.
  * These defines come before including the configuration header files. */
