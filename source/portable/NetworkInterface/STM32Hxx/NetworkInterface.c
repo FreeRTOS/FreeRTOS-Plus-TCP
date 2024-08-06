43ed8513b961bbd6f4f5471ffaf417dbba82dc4e
Gaurav Aggarwal <aggarg@amazon.com>
2024-08-06 17:21:41 +0000

    Code review suggestions
    Signed-off-by: Gaurav Aggarwal <aggarg@amazon.com>

--- source/portable/NetworkInterface/STM32Hxx/NetworkInterface.c
+++ source/portable/NetworkInterface/STM32Hxx/NetworkInterface.c
@@ -87,8 +87,8 @@
     #define niEMAC_HANDLER_TASK_PRIORITY    configMAX_PRIORITIES - 1
 #endif
 
-#ifndef ipconfigEMAC_WATCHDOG_TIMER
-    #define ipconfigEMAC_WATCHDOG_TIMER()
+#ifndef ipconfigEMAC_TASK_HOOK
+    #define ipconfigEMAC_TASK_HOOK()
 #endif
 
 /* Bit map of outstanding ETH interrupt events for processing. */
@@ -1196,7 +1196,7 @@ static void prvEMACHandlerTask( void * pvParameters )
                 prvEthernetUpdateConfig( pdFALSE );
             }
         }
-        ipconfigEMAC_WATCHDOG_TIMER();
+        ipconfigEMAC_TASK_HOOK();
     }
 }
 
