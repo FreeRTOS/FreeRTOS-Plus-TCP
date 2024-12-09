9718ee017dde114bd9fe7fa6eb77df792366fcb9
tony-josi-aws <tonyjosi@amazon.com>
2024-12-09 12:51:14 +0530

    Update version in src files to V4.3.0

--- source/portable/NetworkInterface/MPS4_CS315/NetworkInterface.c
+++ source/portable/NetworkInterface/MPS4_CS315/NetworkInterface.c
@@ -1,5 +1,5 @@
 /*
- * FreeRTOS+TCP <DEVELOPMENT BRANCH>
+ * FreeRTOS+TCP V4.3.0
  * Copyright (C) 2022 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
  * Copyright 2023 Arm Limited and/or its affiliates
  *
