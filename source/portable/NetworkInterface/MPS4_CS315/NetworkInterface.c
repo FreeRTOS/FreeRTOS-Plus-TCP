21c0438b73c7a22f8c7b08d0ff5ac8a5e1e21a97
Tony Josi <tonyjosi@amazon.com>
2025-06-03 22:45:28 +0530

    [V4.3.2] Release updates  (#1257)
    * Fix buffer overflow issue allowing out-of-bounds write when processing LLMNR or mDNS queries with very long DNS names when Buffer Allocation Scheme 1 is used
    
    * [V4.3.2] Release updates
    
    * Fix CI
    
    * [AUTO][RELEASE]: Update version number in source files
    
    * Update SBOM
    
    * Update SBOM
    
    ---------
    
    Co-authored-by: tony-josi-aws <tony-josi-aws@users.noreply.github.com>

--- source/portable/NetworkInterface/MPS4_CS315/NetworkInterface.c
+++ source/portable/NetworkInterface/MPS4_CS315/NetworkInterface.c
@@ -1,5 +1,5 @@
 /*
- * FreeRTOS+TCP V4.3.1
+ * FreeRTOS+TCP V4.3.2
  * Copyright (C) 2022 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
  * Copyright 2023 Arm Limited and/or its affiliates
  *
