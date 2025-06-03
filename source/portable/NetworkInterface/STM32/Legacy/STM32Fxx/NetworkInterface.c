f2f6fdd7477c2b4eaa606c4a8a9dc4375271f41e
tony-josi-aws <tony-josi-aws@users.noreply.github.com>
2025-06-03 16:30:05 +0000

    [AUTO][RELEASE]: Update version number in source files

--- source/portable/NetworkInterface/STM32/Legacy/STM32Fxx/NetworkInterface.c
+++ source/portable/NetworkInterface/STM32/Legacy/STM32Fxx/NetworkInterface.c
@@ -4,7 +4,7 @@
  */
 
 /*
- * FreeRTOS+TCP V4.3.1
+ * FreeRTOS+TCP V4.3.2
  * Copyright (C) 2022 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
  *
  * SPDX-License-Identifier: MIT
--- test/build-combination/AllDisable/FreeRTOSIPConfig.h
+++ test/build-combination/AllDisable/FreeRTOSIPConfig.h
@@ -1,5 +1,5 @@
 /*
- * FreeRTOS+TCP V4.3.1
+ * FreeRTOS+TCP V4.3.2
  * Copyright (C) 2022 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
