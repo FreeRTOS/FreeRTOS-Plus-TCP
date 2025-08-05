038d01939e4d9c3e4bf2d47c447a743f0044b3b9
Posner, Sven <sven.posner@iisb.fraunhofer.de>
2025-08-05 11:32:50 +0200

    Remove xLastUnsolicitedNDTime

--- source/FreeRTOS_ND.c
+++ source/FreeRTOS_ND.c
@@ -97,12 +97,6 @@
 /** @brief The ND cache. */
     static NDCacheRow_t xNDCache[ ipconfigND_CACHE_ENTRIES ];
 
-/** @brief  The time at which the last unsolicited ND was sent. Unsolicited NDs are used
- * to ensure ND tables are up to date and to detect IP address conflicts. */
-/* MISRA Ref 8.9.1 [File scoped variables] */
-/* More details at: https://github.com/FreeRTOS/FreeRTOS-Plus-TCP/blob/main/MISRA.md#rule-89 */
-/* coverity[misra_c_2012_rule_8_9_violation] */
-    static TickType_t xLastUnsolicitedNDTime = 0U;
 
 /*-----------------------------------------------------------*/
 
@@ -1404,20 +1398,5 @@
         return xNeedsNDResolution;
     }
 
-/*-----------------------------------------------------------*/
-
-/**
- * @brief Send an unsolicited ND packet to allow this node to announce the IP-MAC
- *        mapping to the entire network.
- */
-    void vNDSendUnsolicited( void )
-    {
-        /* Setting xLastUnsolicitedNDTime to 0 will force an unsolicited ND the next
-         * time vNDAgeCache() is called. */
-        xLastUnsolicitedNDTime = ( TickType_t ) 0;
-
-        /* Let the IP-task call vARPAgeCache(). */
-        ( void ) xSendEventToIPTask( eNDTimerEvent );
-    }
 /*-----------------------------------------------------------*/
 #endif /* ipconfigUSE_IPv6 */
